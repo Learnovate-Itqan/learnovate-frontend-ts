334fd1fcad2dd0edd18dc43a7b1107c8bfcabe10
Mu-selim <muhamadselim70@gmail.com>
2024-01-24 23:28:22 +0200

    init home page component

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,8 @@
+export const HomePage = () => (
+  <div className="flex h-screen w-full items-center justify-center bg-gradient-to-r from-violet-900 to-sky-900 text-center text-white">
+    <div className="space-y-2">
+      <h1 className="text-6xl font-bold">Learnovate</h1>
+      <p className="text-xl">Coming soon...</p>
+    </div>
+  </div>
+);
