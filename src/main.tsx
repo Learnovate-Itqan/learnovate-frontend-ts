afbbd44ada11a51ca956620da377168adf82a3a3
Mu-selim <muhamadselim70@gmail.com>
2024-01-24 23:28:02 +0200

    Add RouterProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider as ReduxProvider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import { store } from "./redux/store.js";
-import App from "./App.jsx";
+import { Router as router } from "./router.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={new QueryClient()}>
       <ReduxProvider store={store}>
-        <App />
+        <RouterProvider router={router} />
       </ReduxProvider>
     </QueryClientProvider>
   </React.StrictMode>
