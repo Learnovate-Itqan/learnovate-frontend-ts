b6b5756e3d597ceaf8b62835308e0efdbaf54f5c
Mu-selim <muhamadselim70@gmail.com>
2024-01-24 23:27:37 +0200

    Delete App component and add Router component

---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
+import { HomePage } from "./pages/home";
+
+export const Router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route errorElement={<div>404</div>}>
+      <Route path="/">
+        <Route index element={<HomePage />} />
+        <Route path="/about" element={<HomePage />} />
+      </Route>
+      <Route path="/">
+        <Route path="auth/login" element={<>Login</>} />
+        <Route path="auth/register" element={<>Register</>} />
+      </Route>
+      <Route path="*" element={<div>404</div>} />
+    </Route>
+  )
+);
