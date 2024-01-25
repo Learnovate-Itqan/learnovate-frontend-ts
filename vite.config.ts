bff8e4e3d7a202282697e06c282a12cc9ec6a587
Mu-selim <muhamadselim70@gmail.com>
2024-01-25 11:13:30 +0200

    Add alias imports and resolve paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import sitemap from "vite-plugin-sitemap";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), sitemap()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
