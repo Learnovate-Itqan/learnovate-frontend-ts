47683a21f4151ff950f9488d6fbc3f9e296db474
Mu-selim <muhamadselim70@gmail.com>
2024-01-28 19:40:01 +0200

    Refactor FormError component to display multiple error messages

--- src/components/FormError.tsx
+++ src/components/FormError.tsx
@@ -1,14 +1,18 @@
 import { BsExclamationTriangleFill } from "react-icons/bs";
 
 type TFormErrorProps = {
-  message?: string;
+  messages: string[];
 };
 
-export function FromError({ message }: TFormErrorProps) {
+export function FromError({ messages }: TFormErrorProps) {
   return (
-    <div className="flex items-center gap-x-2 rounded-xl bg-red-500/30 p-3 text-sm text-white">
-      <BsExclamationTriangleFill />
-      <p>{message}</p>
-    </div>
+    <ul className={`flex flex-col gap-2 rounded-xl bg-red-500/30 text-sm text-white ${messages.length !== 0 && "p-3"}`}>
+      {messages.map((message, index) => (
+        <li key={index} className="flex gap-x-2.5">
+          <BsExclamationTriangleFill className="translate-y-0.5 text-sm" />
+          <span>{message}</span>
+        </li>
+      ))}
+    </ul>
   );
 }
