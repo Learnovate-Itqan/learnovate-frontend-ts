60df8feab7431e66943da2e400dc80c194a98b91
Mu-selim <muhamadselim70@gmail.com>
2024-01-28 19:43:00 +0200

    Update authErrorSchema to make 'value' property optional

--- src/schemas/authError.ts
+++ src/schemas/authError.ts
@@ -4,7 +4,7 @@ export const authErrorSchema = z.array(
   z.object({
     msg: z.string(),
     type: z.string(),
-    value: z.string(),
+    value: z.string().optional(),
     location: z.string().optional(),
     path: z.string().optional(),
   })
