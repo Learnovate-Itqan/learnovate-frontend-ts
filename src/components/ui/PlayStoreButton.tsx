28902eb805f59c27740c0a4ed31482f3a07a9d60
kareem172 <kareemkhalaf1722@gmail.com>
2024-01-30 01:12:07 +0200

    add stores button

---
+++ src/components/ui/PlayStoreButton.tsx
@@ -0,0 +1,13 @@
+import { FaGooglePlay } from "react-icons/fa";
+
+export function PlayStoreButton() {
+  return (
+    <button className="bg-transparent p-2 border-[1px] min-w-28 border-white rounded-lg flex justify-center items-center gap-4">
+      <FaGooglePlay size={25} />
+      <div className="text-left">
+        <p className="text-xs">GET IT ON</p>
+        <h1 className="font-semibold">Google Play</h1>
+      </div>
+    </button>
+  );
+}
