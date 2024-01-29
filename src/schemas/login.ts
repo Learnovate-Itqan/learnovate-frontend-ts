7b020b77f4707fea684622db7cc2dbd022a38ea0
Mu-selim <muhamadselim70@gmail.com>
2024-01-29 13:40:41 +0200

    Add login response schema

--- src/schemas/login.ts
+++ src/schemas/login.ts
@@ -5,3 +5,12 @@ export const loginSchema = z.object({
   password: z.string().min(1, "password can not be empty"),
   rememberMe: z.boolean().optional(),
 });
+
+export const loginResponseSchema = z.object({
+  accessToken: z.string(),
+  data: z.object({
+    email: z.string().email(),
+    id: z.string().uuid(),
+    name: z.string(),
+  }),
+});
