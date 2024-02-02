fce6728b99b0aa348fc9ab22ed18013495a55ee1
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-02 15:27:59 +0200

    add navbar slice

---
+++ src/redux/slices/navSlice.ts
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isOpen: false,
+};
+
+const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducers: {
+    toggleNav: (state) => {
+      state.isOpen = !state.isOpen;
+    },
+  },
+});
+
+export const { toggleNav } = navSlice.actions;
+export const navReducer = navSlice.reducer;
