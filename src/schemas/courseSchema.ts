ce426371a16b54d9edee850d637f3de905cf2132
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-03 22:19:53 +0200

    add mentor schema and course schema

---
+++ src/schemas/courseSchema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const courseSchema = z.object({
+  id: z.string().uuid(),
+  title: z.string(),
+  publishTime: z.string(),
+  description: z.string(),
+  keywords: z.array(z.string()),
+  image: z.string().url(),
+  progress: z.number(),
+  estimatedTime: z.number(),
+  noChapters: z.number(),
+  noStudentsEnrolled: z.number(),
+  rating: z.number(),
+  cLevel: z.string(),
+  cLink: z.string(),
+  trackID: z.string().uuid(),
+});
