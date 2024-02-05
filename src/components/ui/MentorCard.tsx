c3b28d630bcab2103ee48d80e2999206e9edf1d3
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-05 05:38:20 +0200

    fix styling issues on mentor section and mentor card

--- src/components/ui/MentorCard.tsx
+++ src/components/ui/MentorCard.tsx
@@ -1,6 +1,8 @@
 import { HiStar } from "react-icons/hi2";
 import { Link } from "react-router-dom";
 
+import { Tag } from "./Tag";
+
 type MentorCardProps = {
   name: string;
   image?: string | undefined;
@@ -12,17 +14,17 @@ type MentorCardProps = {
 
 export default function MentorCard({ name, id, rating, image, title, className }: MentorCardProps) {
   return (
-    <div className={"relative rounded-lg shadow-lg overflow-hidden w-52 h-72  flex flex-col " + className}>
-      <img src={image} alt="" className="bg-[#B7B9C3] object-cover pb-0 grow" />
+    <div className={"relative rounded-lg shadow-lg overflow-hidden flex flex-col " + className}>
+      <img src={image} alt={`${name} image`} title={`${name}`} className="bg-[#B7B9C3] w-full object-cover pb-0 grow" />
       <div className="absolute -left-1 -top-1 flex w-full justify-between p-4 text-sm">
-        <span className="flex items-center justify-center gap-1 rounded-lg text-black bg-[#E5E7ED] px-2 py-1">
+        <Tag>
           <HiStar className="inline-block h-5 w-5 text-yellow-500" />
           <p>{rating}</p>
-        </span>
+        </Tag>
       </div>
       <Link to={`/mentors/${id}`} className="bg-[#222C54] py-3  min-h-fit flex flex-col justify-center items-center">
         <h1 className="text-xl text-center font-semibold">{name || "fake Mentor"}</h1>
-        <p className="text-sm text-center whitespace-nowrap">{title || "fake title"}</p>
+        <p className=" text-center whitespace-nowrap">{title || "fake title"}</p>
       </Link>
     </div>
   );
--- src/pages/home/MentorSection.tsx
+++ src/pages/home/MentorSection.tsx
@@ -12,21 +12,21 @@ export default function MentorSection({ mentors }: { mentors: z.infer<typeof men
   if (!mentors) return null;
 
   return (
-    <section className=" flex flex-col gap-5 justify-evenly items-center bg-dark-navy py-8 min-h-dvh text-white">
-      <aside className="flex flex-col justify-center gap-6 text-center items-center mx-12 md:mx-24 lg:mx-48">
+    <section className="flex flex-col gap-16 py-20 justify-evenly items-center bg-dark-navy text-white">
+      <aside className="container flex flex-col justify-center gap-6 text-center items-center max-w-screen-xl ">
         <h1 className="text-4xl font-semibold">Our top mentors</h1>
-        <p className="text-neutral-gray leading-6">
+        <p className=" text-gray-300 text-balance leading-6">
           Lorem ipsum dolor sit amet. Sit facere dignissimos et rerum ducimus non nihil consequatur est sapiente illo in
           ipsum repellendus et dolores velit. Vel Quis velit et fuga nostrum ut ipsum beatae ea tenetur soluta et illum
           ducimus! Id sint galisum et corrupti obcaecati qui quisquam quam hic sint aliquam nam Quis porro qui velit
           soluta et eius natus.
         </p>
-        <div className="min-w-11 hidden sm:block">
-          <Button text="Discover More" type="button" />
+        <div className="min-w-11 w-48 hidden sm:block">
+          <Button text="Discover More" type="button" onClick={() => navigate("/mentors")} />
         </div>
       </aside>
-      <aside className="flex flex-col gap-10 justify-center  sm:mx-8 ">
-        <main className="flex flex-wrap gap-10 justify-center">
+      <aside className="container flex flex-col gap-10 justify-center ">
+        <main className=" container grid grid-cols-auto-fit xl:grid-cols-5 gap-10 justify-center">
           {mentors.map((mentor) => (
             <MentorCard
               key={mentor.id}
@@ -39,7 +39,7 @@ export default function MentorSection({ mentors }: { mentors: z.infer<typeof men
             />
           ))}
         </main>
-        <div className="min-w-11 mx-8 sm:hidden">
+        <div className="container min-w-11 sm:hidden">
           <Button text="Discover More" type="button" onClick={() => navigate("/mentors")} />
         </div>
       </aside>
