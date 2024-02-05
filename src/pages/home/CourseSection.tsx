16fc1613e98fd314151dc970a2218b4c9ed3d33b
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-05 06:24:36 +0200

    fix styling issues on course section

--- src/pages/home/CourseSection.tsx
+++ src/pages/home/CourseSection.tsx
@@ -12,18 +12,16 @@ export default function CourseSection({ courses }: { courses: z.infer<typeof cou
   if (!courses) return null;
   return (
     <>
-      <section className="mx-5 sm:mx-10 md:mx-20 mt-16">
+      <section className="container py-20">
         <header className="flex flex-col gap-4 sm:flex-row justify-between items-center">
           <h1 className="text-dark-navy text-3xl font-semibold">Start With our Courses</h1>
-          <div className="hidden sm:block">
-            <Button text="Discover All" type="button" />
-          </div>
+          <Button className="hidden max-w-36 sm:block" text="Discover All" type="button" />
         </header>
-        <main className="flex flex-wrap gap-5 my-10">
+        <main className="grid grid-cols-auto-fit-20 xl:grid-cols-3 gap-5 my-10">
           {courses.map((course) => (
             <CourseCard
               key={course.id}
-              className="w-96"
+              className=""
               name={course.title}
               rate={course.rating}
               description={course.description}
@@ -40,7 +38,7 @@ export default function CourseSection({ courses }: { courses: z.infer<typeof cou
           <Button text="Discover All" type="button" />
         </div>
       </section>
-      <section className="flex flex-col md:flex-row gap-5 justify-evenly md:my-44 my-24 mx-5 items-center">
+      <section className="container mb-20 flex flex-col md:flex-row gap-5 justify-evenly items-center">
         <aside className="md:max-w-96 max-w-72">
           <img src={mentor} alt="" className="" />
         </aside>
@@ -48,7 +46,7 @@ export default function CourseSection({ courses }: { courses: z.infer<typeof cou
           <h1 className="text-3xl font-semibold text-dark-navy">
             Be a <span className="text-royal-blue">Mentor</span>
           </h1>
-          <p className="leading-6 text-neutral-gray ">
+          <p className="leading-6  text-neutral-gray ">
             Lorem ipsum dolor sit amet. Sit facere dignissimos et rerum ducimus non nihil consequatur est sapiente illo
             in ipsum repellendus et dolores velit. Vel Quis velit et fuga nostrum ut ipsum beatae ea tenetur soluta et
             illum ducimus!{" "}
