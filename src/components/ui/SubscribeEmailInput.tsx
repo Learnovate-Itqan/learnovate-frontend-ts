74bbc0831e4176d48ff1a16175e0b1654fae7995
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-05 22:32:53 +0200

    fix arwow padding in the subscribtionEmailBox

--- src/components/ui/SubscribeEmailInput.tsx
+++ src/components/ui/SubscribeEmailInput.tsx
@@ -3,9 +3,9 @@ import { FaArrowRight } from "react-icons/fa";
 export function SubscribeEmailInput() {
   return (
     <div className="flex overflow-hidden rounded-md w-fit">
-      <input type="text" placeholder="Email" className=" p-2 outline-none" />
-      <button className="bg-royal-blue p-2">
-        <FaArrowRight size={22} />
+      <input type="text" placeholder="Email" className="text-dark-navy p-2 outline-none" />
+      <button className="bg-royal-blue py-2 px-3">
+        <FaArrowRight size={18} />
       </button>
     </div>
   );
