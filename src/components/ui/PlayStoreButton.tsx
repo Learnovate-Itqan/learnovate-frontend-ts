db86bcc17ea91201bdec2664a2966701fe325eb6
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-05 03:33:46 +0200

    fix layout issus in hero section

--- src/components/ui/PlayStoreButton.tsx
+++ src/components/ui/PlayStoreButton.tsx
@@ -1,9 +1,11 @@
-import { FaGooglePlay } from "react-icons/fa";
+import { GooglePlayLogo } from "../icons/GooglePlayLogo";
 
 export function PlayStoreButton() {
   return (
-    <button className="bg-transparent p-2 border-[1px] min-w-40 border-white rounded-lg flex justify-center items-center gap-4">
-      <FaGooglePlay size={25} />
+    <button className="md:bg-dark-navy/30 p-2 border-[1px] min-w-40 border-white rounded-lg flex justify-center items-center gap-4">
+      <span className="w-10 aspect-square">
+        <GooglePlayLogo />
+      </span>
       <div className="text-left">
         <p className="text-xs">GET IT ON</p>
         <h1 className="font-semibold">Google Play</h1>
--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -5,29 +5,29 @@ import { PlayStoreButton } from "@/components/ui/PlayStoreButton";
 
 export default function HeroSection() {
   return (
-    <section className="relative min-h-dvh px-5 sm:px-10 md:px-20 bg-gradient-to-b from-dark-navy via-dark-navy/90 to-dark-navy/80 md:from-transparent md:to-transparent md:via-transparent md:bg-dark-navy flex justify-start items-center">
+    <section className="relative py-24 min-h-dvh bg-gradient-to-b from-dark-navy via-dark-navy/90 to-dark-navy/80 md:from-transparent md:to-transparent md:via-transparent md:bg-dark-navy flex justify-start items-center">
       <div
         style={{
           backgroundImage: `url(${Hero})`,
         }}
         className="hidden md:block bg-dark-navy inset-0 absolute z-10 bg-cover bg-right bg-no-repeat "
       />
-      <main className=" text-white md:w-2/3 lg:w-1/2 grid gap-8 z-20">
-        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold drop-shadow-xl ">
+      <main className=" container  text-white text-center md:text-left grid gap-5 xs:gap-8 z-20">
+        <h1 className=" text-4xl md:text-5xl lg:text-5xl xl:text-6xl font-bold ">
           <span className="block">Choose your </span>
           <span className="block">
             <span className=" text-royal-blue">tech</span> road with
           </span>
           <span className="block"> industry experts </span>
         </h1>
-        <p className="sm:w-2/3 leading-6">
+        <p className="md:w-2/3 text-balance leading-6 max-w-[750px]">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis elit a arcu laoreet cursus eu sit amet
           ipsum.{" "}
         </p>
-        <div className="w-1/4 min-w-36">
+        <div className="md:w-1/4 md:max-w-48 min-w-36">
           <Button text="Get Start" type="button" />
         </div>
-        <div className="flex gap-2 sm:gap-4">
+        <div className="flex flex-col xs:flex-row gap-2 justify-center md:justify-start sm:gap-4">
           <AppStoreButton />
           <PlayStoreButton />
         </div>
