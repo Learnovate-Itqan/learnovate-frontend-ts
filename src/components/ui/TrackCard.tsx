0d2335b4cdd9a4846eecba54620e5570ca607985
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-05 05:02:00 +0200

    fix styling issues on track section (track opacity, padding top)

--- src/components/ui/TrackCard.tsx
+++ src/components/ui/TrackCard.tsx
@@ -8,11 +8,14 @@ type TrackCardProps = {
 export default function TrackCard({ name, image }: TrackCardProps) {
   return (
     <Link
+      style={{ backgroundImage: `url(${image})` }}
       to={`/track/${name.replace(" ", "-")}`}
-      className="relative min-w-fit py-3 px-10 overflow-hidden rounded-lg bg-dark-navy"
+      className="relative min-w-fit py-3 px-10 bg-cover bg-opacity-20 overflow-hidden rounded-lg"
     >
-      <img className=" absolute inset-0 object-cover opacity-20" src={image} alt="" loading="lazy" />
-      <h1 className="text-white text-xl font-semibold z-10 opacity-100">{name}</h1>
+      <h1 className="text-white text-sm md:text-lg lg:text-xl absolute font-semibold flex justify-center items-center z-10 bg-dark-navy/60 inset-0 opacity-100">
+        {name}
+      </h1>
+      <h1 className="text-sm md:text-lg lg:text-xl font-semibold opacity-0">{name}</h1>
     </Link>
   );
 }
--- src/pages/home/TrackSection.tsx
+++ src/pages/home/TrackSection.tsx
@@ -6,16 +6,16 @@ import { trackSchema } from "@/schemas/trackSchema";
 export default function TrackSection({ tracks }: { tracks: z.infer<typeof trackSchema>[] }) {
   if (!tracks) return null;
   return (
-    <section className="flex justify-center items-center min-h-dvh  text-center">
-      <main className="m-8 md:w-3/4 lg:w-2/3 flex flex-col gap-6">
-        <h1 className="text-4xl text-dark-navy font-semibold ">Start your journey with your track</h1>
-        <p className="text-neutral-gray leading-6 ">
+    <section className="container py-20 flex justify-center items-center text-center">
+      <main className="flex flex-col gap-6 max-w-screen-xl">
+        <h1 className="text-4xl text-dark-navy text-balance  font-semibold ">Start your journey with your track</h1>
+        <p className="text-neutral-gray text-balance leading-6 ">
           Lorem ipsum dolor sit amet. Sit facere dignissimos et rerum ducimus non nihil consequatur est sapiente illo in
           ipsum repellendus et dolores velit. Vel Quis velit et fuga nostrum ut ipsum beatae ea tenetur soluta et illum
           ducimus! Id sint galisum et corrupti obcaecati qui quisquam quam hic sint aliquam nam Quis porro qui velit
           soluta et eius natus.
         </p>
-        <div className="flex flex-wrap gap-4 justify-center items-center mx-7">
+        <div className="flex flex-wrap gap-2 md:gap-4 justify-center items-center ">
           {tracks.map((track) => (
             <TrackCard key={track.id} {...track} />
           ))}
