21b3cc08d5741288a14f22d607bd6566bc78961f
Mu-selim <muhamadselim70@gmail.com>
2024-02-07 23:29:54 +0200

    Update UI components and integrate Google login

--- src/components/ui/SocialButton.tsx
+++ src/components/ui/SocialButton.tsx
@@ -1,19 +1,51 @@
+import { useGoogleLogin } from "@react-oauth/google";
 import { FcGoogle } from "react-icons/fc";
+
+import { Button } from "@/components/ui/button";
+import { globalResponseFormat, postRequest } from "@/hooks/useApi";
 
 type TSocialButtonProps = {
   text: string;
-  onClick: () => void;
+  setIsModalOpen: (value: boolean) => void;
+  setError: (value: string[]) => void;
+  disabled?: boolean;
 };
 
-export function SocialButton({ text, onClick }: TSocialButtonProps) {
+export function SocialButton({ text, setIsModalOpen, setError, disabled }: TSocialButtonProps) {
+  const googleAuth = useGoogleLogin({
+    flow: "auth-code",
+    onSuccess: async (codeResponse) => {
+      console.log(codeResponse);
+      const { code: token } = codeResponse;
+      const data = await postRequest("/auth/continue-with-google", { token });
+      const response = globalResponseFormat(data);
+
+      if (response.status === "failed") {
+        // setError(["Something went wrong!"]);
+        console.log({ errorData: response.data });
+        setIsModalOpen(true);
+        return;
+      }
+
+      console.log(response.data);
+    },
+    onError: (error) => {
+      console.log(error);
+      setError(["Something went wrong!"]);
+    },
+  });
+
   return (
-    <button
+    <Button
       type="button"
-      onClick={onClick}
-      className="flex w-full items-center justify-center gap-2 rounded-xl border-[0.1rem] border-zinc-500 bg-transparent px-4 py-2.5 font-medium leading-snug transition-colors duration-300 ease-cubic hover:bg-royal-blue/10"
+      onClick={googleAuth}
+      variant="outline"
+      size="lg"
+      disabled={disabled}
+      className="w-full bg-transparent border border-gray-300 text-gray-600 hover:bg-gray-100 hover:border-gray-400"
     >
       <FcGoogle className="text-2xl" />
       <span>{text}</span>
-    </button>
+    </Button>
   );
 }
