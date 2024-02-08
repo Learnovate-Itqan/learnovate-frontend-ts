cd7f670c61791d1ff094ecfd4d2b3d1468f2028d
Mu-selim <muhamadselim70@gmail.com>
2024-02-08 20:00:15 +0200

    Add MentorInfo component and update layout

--- src/pages/mentor/index.tsx
+++ src/pages/mentor/index.tsx
@@ -1,5 +1,6 @@
 import { mentor } from "@/db/mentor";
 
+import { MentorInfo } from "./components/mentorInfo";
 import { MentorSmallCalendar } from "./components/mentorSmallCalendar";
 import { ViewerHeader } from "./components/mentorViewHeader";
 import { SkillsBox } from "./components/skillsBox";
@@ -8,10 +9,11 @@ export const MentorPage = () => {
   return (
     <>
       <ViewerHeader {...mentor} />
-      <main className="w-full py-10">
+      <main className="w-full py-10 *:text-dark-navy">
         <div className="container">
           <SkillsBox skills={mentor.skills} />
-          <div className="my-8">
+          <div className="my-8 flex flex-col sm:flex-row gap-y-8 gap-x-4 justify-between">
+            <MentorInfo {...mentor} />
             <MentorSmallCalendar />
           </div>
         </div>
