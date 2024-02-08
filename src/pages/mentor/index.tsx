e0a4aa290d6eeff3e019eda96f553a19d3b67250
Mu-selim <muhamadselim70@gmail.com>
2024-02-08 17:53:19 +0200

    Add MentorSmallCalendar and SkillsBox components

--- src/pages/mentor/index.tsx
+++ src/pages/mentor/index.tsx
@@ -1,11 +1,21 @@
 import { mentor } from "@/db/mentor";
 
+import { MentorSmallCalendar } from "./components/mentorSmallCalendar";
 import { ViewerHeader } from "./components/mentorViewHeader";
+import { SkillsBox } from "./components/skillsBox";
 
 export const MentorPage = () => {
   return (
     <>
       <ViewerHeader {...mentor} />
+      <main className="w-full py-10">
+        <div className="container">
+          <SkillsBox skills={mentor.skills} />
+          <div className="my-8">
+            <MentorSmallCalendar />
+          </div>
+        </div>
+      </main>
     </>
   );
 };
