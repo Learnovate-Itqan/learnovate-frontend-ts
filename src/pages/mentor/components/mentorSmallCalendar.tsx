59b91dbb831bd429c753bc49f4f9ad99577e4776
Mu-selim <muhamadselim70@gmail.com>
2024-02-08 19:59:53 +0200

    Refactor mentorSmallCalendar component layout

--- src/pages/mentor/components/mentorSmallCalendar.tsx
+++ src/pages/mentor/components/mentorSmallCalendar.tsx
@@ -8,7 +8,7 @@ import { CalendarCarousel } from "./CalendarCarousel";
 export const MentorSmallCalendar = () => {
   const [date, setDate] = useState<Date | undefined>(new Date());
   return (
-    <div className="border p-4 rounded-lg max-w-[18rem] shadow-lg space-y-4">
+    <div className="border p-4 rounded-lg max-w-[18rem] flex-col lg:flex-row lg:max-w-fit gap-x-4 xl:gap-x-8 flex shadow-lg space-y-4 lg:space-y-0">
       <div className="w-fit">
         <h5 className="text-xl font-medium pb-2.5">Availability:</h5>
         <Calendar
@@ -22,11 +22,13 @@ export const MentorSmallCalendar = () => {
           fixedWeeks
         />
       </div>
-      <div className="space-y-3">
-        <h5 className="text-xl font-medium pb-2.5">Schedule:</h5>
-        <CalendarCarousel />
+      <div className="space-y-4 max-w-[18rem]">
+        <div className="space-y-3">
+          <h5 className="text-xl font-medium pb-2.5">Schedule:</h5>
+          <CalendarCarousel />
+        </div>
+        <Button className="w-full">Book a session</Button>
       </div>
-      <Button className="w-full">Book a session</Button>
     </div>
   );
 };
