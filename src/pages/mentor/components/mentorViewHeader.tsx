588d18b7cc10c593ad583d897f7c5cccaa55466b
Mu-selim <muhamadselim70@gmail.com>
2024-02-10 16:33:21 +0200

    Refactor mentorViewHeader component layout

--- src/pages/mentor/components/mentorViewHeader.tsx
+++ src/pages/mentor/components/mentorViewHeader.tsx
@@ -17,10 +17,18 @@ export const ViewerHeader = ({ name, jobTitle, description, image, price }: TVie
         <div className="absolute inset-0 -z-20">
           <img src={mentorViewer} alt={name} loading="lazy" className="h-full w-full object-cover" />
         </div>
-        <div className="z-0 text-white py-20 lg:pb-0 lg:pt-20">
-          <div className="container h-full flex gap-y-4 flex-col-reverse sm:flex-row sm:items-center lg:items-end justify-between">
-            <div className="h-full flex flex-col justify-center lg:mb-20 sm:gap-2">
-              <h1 className="font-semibold text-royal-blue text-3xl xs:text-4xl sm:text-5xl text-pretty">{name}</h1>
+        <div className="z-0 text-white py-20">
+          <div className="container h-full flex gap-x-10 gap-y-4 flex-col sm:flex-row sm:items-center">
+            <div className="w-fit rounded-full overflow-hidden border-b-[0.1rem] border-b-royal-blue bg-gradient-to-t from-royal-blue/80 via-transparent to-transparent">
+              <img
+                src={image}
+                alt={`${name} - ${description}`}
+                title={name}
+                className="h-52 sm:w-auto max-h-72 sm:h-full object-cover"
+              />
+            </div>
+            <div className="h-full flex flex-col justify-center sm:gap-2">
+              <h1 className="font-semibold text-royal-blue text-3xl xs:text-4xl md:text-5xl text-pretty">{name}</h1>
               <h2 className="font-medium text-xl text-pretty">{jobTitle}</h2>
               <p className="max-w-lg text-balance mt-2">{description}</p>
               <div className="flex flex-col xs:flex-row sm:flex-col mt-4 gap-y-2 gap-x-4">
@@ -29,14 +37,6 @@ export const ViewerHeader = ({ name, jobTitle, description, image, price }: TVie
                   <span>{`Price: ${price}$ / hour`}</span>
                 </div>
               </div>
-            </div>
-            <div className="flex w-fit items-end rounded-full overflow-hidden border-b-2 border-b-royal-blue lg:border-b-0 lg:overflow-visible">
-              <img
-                src={image}
-                alt={`${name} - ${description}`}
-                title={name}
-                className="w-52 h-52 sm:w-auto sm:h-full object-cover"
-              />
             </div>
           </div>
         </div>
