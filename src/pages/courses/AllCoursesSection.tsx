670fdd5072aeec91c84496a3b05a97c23abbdf4a
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-12 06:18:35 +0200

    Refactor AllCoursesSection component to include new features and handle errors

--- src/pages/courses/AllCoursesSection.tsx
+++ src/pages/courses/AllCoursesSection.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { TbAdjustmentsFilled } from "react-icons/tb";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { z } from "zod";
 
 import courseImage from "@/assets/learnovate-thumbnail-course.png";
+import { Button } from "@/components/ui/Button";
 import CourseCard from "@/components/ui/CourseCard";
 import Modal from "@/components/ui/Modal";
 import { Paginate } from "@/components/ui/Paginate";
 import { SearchBar } from "@/components/ui/SearchBar";
+import { Spinner } from "@/components/ui/Spinner";
+import { useGetData } from "@/hooks/useApi";
+import { courseSchema } from "@/schemas/courseSchema";
 
 import { FilterCoursesForm } from "./FilterCoursesForm";
 
@@ -14,13 +19,41 @@ const Tracks = ["All", "Data Science", "Dev Ops", "Computer Science", "IOS", "Em
 
 export function AllCoursesSection() {
   const [searchParams, setSearchParams] = useSearchParams();
-
+  const navigate = useNavigate();
+  const { data: response } = useGetData(`courses${window.location.search}`);
+  const { data } = response || {};
+  const { courses, status } = data || {};
+  console.log(data);
   const selectedTrack = searchParams.get("track") || "all";
 
   const handleTrackChange = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (e.currentTarget.value === "all") {
+      searchParams.delete("track");
+      setSearchParams(searchParams, { replace: true });
+      return;
+    }
+    if (e.currentTarget.value.toLowerCase() === selectedTrack) {
+      searchParams.delete("track");
+      setSearchParams(searchParams, { replace: true });
+      return;
+    }
     searchParams.set("track", e.currentTarget.value);
     setSearchParams(searchParams, { replace: true });
   };
+
+  const handleDeletingAllFilters = () => {
+    navigate("/courses", { replace: true });
+  };
+
+  if (status === "failed") {
+    return (
+      <div className="flex flex-col items-center justify-center gap-3 py-20 ">
+        <h1 className="text-3xl font-semibold ">Something went wrong</h1>
+        <p className="text-xl text-dark-navy font-semibold"> please try again</p>
+        <Button className="max-w-48 mt-5" text="Try Again" type="button" onClick={() => navigate("/")} />
+      </div>
+    );
+  }
 
   return (
     <main className=" py-20">
@@ -29,8 +62,8 @@ export function AllCoursesSection() {
           {Tracks.map((track, index) => (
             <button
               key={index}
-              value={track.replace(" ", "-").toLowerCase()}
-              className={` border-2 whitespace-nowrap transition-colors border-royal-blue px-4 py-2 rounded-xl ${track.replace(" ", "-").toLowerCase() === selectedTrack ? "bg-royal-blue text-white" : "text-royal-blue"}`}
+              value={track.toLowerCase()}
+              className={` border-2 whitespace-nowrap transition-colors border-royal-blue px-4 py-2 rounded-xl ${track.toLowerCase() === selectedTrack.toLowerCase() ? "bg-royal-blue text-white" : "text-royal-blue"}`}
               onClick={handleTrackChange}
             >
               {track}
@@ -57,24 +90,35 @@ export function AllCoursesSection() {
           </Modal>
         </div>
       </header>
-      <main className="px-1 xs:container grid grid-cols-auto-fit-22 xl:grid-cols-3 gap-5 py-10">
-        {Array.from({ length: 9 }, (_, index) => (
-          <CourseCard
-            key={index}
-            id={`${index}`}
-            name={"Artificial Intelligence"}
-            track={"Data Science"}
-            duration={120}
-            level={"Intermediate"}
-            rate={5}
-            image={courseImage}
-            price={index * 100}
-            description={
-              "This course provides an introduction to the field of Artificial Intelligence and its applications. Students will gain a solid foundation in the principles and techniques. "
-            }
-          />
-        ))}
-      </main>
+      {!courses ? (
+        <div className="py-20 flex justify-center items-center">
+          <Spinner />
+        </div>
+      ) : courses.length === 0 ? (
+        <div className="container py-20 w-full flex justify-center items-center">
+          <section className="flex flex-col gap-3">
+            <h1 className="text-3xl font-bold text-center">No courses available</h1>
+            <Button text="Clear Filters" type="button" onClick={handleDeletingAllFilters} />
+          </section>
+        </div>
+      ) : (
+        <main className="px-1 xs:container grid grid-cols-auto-fit-22 xl:grid-cols-3 gap-5 py-10">
+          {courses?.map((course: z.infer<typeof courseSchema>) => (
+            <CourseCard
+              key={course.id}
+              id={course.id}
+              name={course.title}
+              track={"Data Science"}
+              duration={course.estimatedTime}
+              level={course.cLevel}
+              rate={course.rating}
+              image={course.image || courseImage}
+              price={course.price}
+              description={course.description}
+            />
+          ))}
+        </main>
+      )}
       <Paginate pageCount={10} />
     </main>
   );
