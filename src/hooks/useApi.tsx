746abc38b51e78d7124c26dad98ea01a2165d785
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-13 19:51:54 +0200

    change api base url for testing

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -8,7 +8,7 @@ const token = localStorage.getItem("token");
 const decryptedToken = token ? decrypt(token, import.meta.env.VITE_TOKEN_SECRET) : "";
 
 export const api: AxiosInstance = axios.create({
-  baseURL: "https://learnovate-server.onrender.com/api/v1",
+  baseURL: "https://learnovate-5zjn.onrender.com/api/v1",
 
   withCredentials: true,
   headers: {
