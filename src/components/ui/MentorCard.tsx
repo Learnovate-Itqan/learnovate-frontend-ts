782a2cffa45353a831eda3c8dc5863044ca90a45
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-15 01:09:32 +0200

    Refactor styling of MentorCard component

--- src/components/ui/MentorCard.tsx
+++ src/components/ui/MentorCard.tsx
@@ -14,22 +14,24 @@ type MentorCardProps = {
 
 export default function MentorCard({ name, id, rating, image, title, className }: MentorCardProps) {
   return (
-    <div className={"relative rounded-lg shadow-lg overflow-hidden flex flex-col " + className}>
-      <img
-        src={image}
-        alt={`${name} image`}
-        title={`${name}`}
-        className="bg-[#B7B9C3] min-h-56 aspect-square w-full object-cover pb-0 grow"
-      />
-      <div className="absolute -left-1 -top-1 flex w-full justify-between p-4 text-sm">
-        <Tag>
-          <HiStar className="inline-block h-5 w-5 text-yellow-500" />
-          <p>{rating.toFixed(1)}</p>
-        </Tag>
+    <div className={"rounded-lg shadow-lg overflow-hidden flex flex-col " + className}>
+      <div className="relative overflow-hidden">
+        <img
+          src={image}
+          alt={`${name} image`}
+          title={`${name}`}
+          className="bg-[#B7B9C3] aspect-square w-full h-full object-cover pb-0"
+        />
+        <div className="absolute -left-1 -top-1 flex w-full justify-between p-4 text-sm">
+          <Tag>
+            <HiStar className="inline-block h-5 w-5 text-yellow-500" />
+            <p>{rating.toFixed(1)}</p>
+          </Tag>
+        </div>
       </div>
-      <Link to={`/mentors/${id}`} className="bg-[#222C54] py-3  min-h-fit flex flex-col justify-center items-center">
-        <h1 className="text-xl text-center font-semibold">{name || "fake Mentor"}</h1>
-        <p className=" text-center whitespace-nowrap">{title || "fake title"}</p>
+      <Link to={`/mentors/${id}`} className="bg-[#222C54] py-2  min-h-fit flex flex-col justify-center items-center">
+        <h1 className="text-base sm:text-xl text-center font-semibold">{name || "fake Mentor"}</h1>
+        <p className=" text-center text-sm sm:text-base whitespace-nowrap">{title || "fake title"}</p>
       </Link>
     </div>
   );
