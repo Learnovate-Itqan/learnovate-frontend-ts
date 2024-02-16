526d865ab0e9f616fa67e553569acb2e873939f7
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-16 15:52:36 +0200

    Refactor track page component and add error handling and page skelton

--- src/pages/track/index.tsx
+++ src/pages/track/index.tsx
@@ -1,10 +1,14 @@
+import { useNavigate, useParams } from "react-router-dom";
+
 import mentorImage from "@/assets/home/Mentor.png";
 import courseImage from "@/assets/learnovate-thumbnail-course.png";
 import CourseCard from "@/components/ui/CourseCard";
 import MentorCard from "@/components/ui/MentorCard";
 import { Tag } from "@/components/ui/Tag";
 import { Button } from "@/components/ui/button";
+import { useGetData } from "@/hooks/useApi";
 
+import { PageSkelton } from "./PageSkelton";
 import { TrackHeader } from "./TrackHeader";
 
 const TRACK = {
@@ -33,75 +37,99 @@ const TRACK = {
 };
 
 export function Track() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  // fetch Track Data
+  const { data: response } = useGetData(`/track/${id}`);
+  const { data, status } = response || {};
+  const { track } = data || {};
+
+  if (status === "failed" && !TRACK) {
+    return (
+      <div className="flex flex-col items-center justify-center gap-3 py-20 ">
+        <h1 className="text-3xl font-semibold ">Something went wrong</h1>
+        <p className="text-xl text-dark-navy font-semibold"> please try again</p>
+        <Button className="max-w-48 w-48 text-base mt-5" type="button" onClick={() => navigate("/")}>
+          Try again
+        </Button>
+      </div>
+    );
+  }
+  console.log(track);
+
   return (
     <>
-      <TrackHeader track={TRACK} />
-      <main className="container ">
-        <header className="flex flex-col gap-10 py-10">
-          <div className="flex md:gap-4 gap-2 flex-wrap">
-            {TRACK.relatedTopics.map((topic, index) => (
-              <Tag
-                title={topic}
-                key={index}
-                className="bg-white capitalize border-royal-blue text-royal-blue border-[1px] font-[400]"
-              >
-                {topic}
-              </Tag>
-            ))}
-          </div>
-          <section className="flex flex-col lg:flex-row gap-6 justify-between">
-            <aside className="flex flex-col gap-2">
-              <h4 className="font-semibold text-lg">Description:</h4>
-              <p className="max-w-[900px] leading-5">{TRACK.description}</p>
-            </aside>
-            <aside className="flex w-96 flex-col gap-2">
-              <h4 className="font-semibold text-lg">Estimated time:</h4>
-              <p className="max-w-[850px] leading-5">{TRACK.estimatedTime}</p>
-              <Button className="max-w-60" type="button">
-                Show Roadmap
-              </Button>
-            </aside>
-          </section>
-        </header>
-        <main className="mb-16 grid gap-10">
-          <section className="flex flex-col gap-3">
-            <h3 className="text-2xl font-semibold">Ordered Courses:</h3>
-            <div className="grid grid-cols-auto-fit-19 gap-3">
-              {Array.from({ length: 3 }).map((_, index) => (
-                <CourseCard
-                  className="max-w-[29rem]"
+      <TrackHeader track={track || TRACK} />
+      {!track && !TRACK ? (
+        <PageSkelton />
+      ) : (
+        <main className="container ">
+          <header className="flex flex-col gap-10 py-10">
+            <div className="flex md:gap-4 gap-2 flex-wrap">
+              {TRACK.relatedTopics.map((topic, index) => (
+                <Tag
+                  title={topic}
                   key={index}
-                  name="Python for Data Science"
-                  description="Learn Python for Data Science and Machine Learning"
-                  rate={4.5}
-                  track="UI/UX"
-                  duration={50}
-                  level="intermediate"
-                  image={courseImage}
-                  price={20}
-                  id={`${index}`}
-                />
+                  className="bg-white capitalize border-royal-blue text-royal-blue border-[1px] font-[400]"
+                >
+                  {topic}
+                </Tag>
               ))}
             </div>
-          </section>
-          <section className="flex flex-col gap-3">
-            <h3 className="text-2xl font-semibold">Related Mentors:</h3>
-            <div className="flex justify-start flex-wrap gap-2 sm:gap-3">
-              {Array.from({ length: 5 }).map((_, index) => (
-                <MentorCard
-                  className="w-[150px] h-[200px] sm:w-[250px] sm:h-[300px]"
-                  key={index}
-                  name="Khalid Ahmed"
-                  title="UI/UX Designer"
-                  rating={4.5}
-                  image={mentorImage}
-                  id={`${index}`}
-                />
-              ))}
-            </div>
-          </section>
+            <section className="flex flex-col lg:flex-row gap-6 justify-between">
+              <aside className="flex flex-col gap-2">
+                <h4 className="font-semibold text-lg">Description:</h4>
+                <p className="max-w-[900px] leading-5">{TRACK.description}</p>
+              </aside>
+              <aside className="flex w-96 flex-col gap-2">
+                <h4 className="font-semibold text-lg">Estimated time:</h4>
+                <p className="max-w-[850px] leading-5">{TRACK.estimatedTime}</p>
+                <Button className="max-w-60" type="button">
+                  Show Roadmap
+                </Button>
+              </aside>
+            </section>
+          </header>
+          <main className="mb-16 grid gap-10">
+            <section className="flex flex-col gap-3">
+              <h3 className="text-2xl font-semibold">Ordered Courses:</h3>
+              <div className="grid grid-cols-auto-fit-19 gap-3">
+                {Array.from({ length: 3 }).map((_, index) => (
+                  <CourseCard
+                    key={index}
+                    name="Python for Data Science"
+                    description="Learn Python for Data Science and Machine Learning"
+                    rate={4.5}
+                    track="UI/UX"
+                    duration={50}
+                    level="intermediate"
+                    image={courseImage}
+                    price={20}
+                    id={`${index}`}
+                  />
+                ))}
+              </div>
+            </section>
+            <section className="flex flex-col gap-3">
+              <h3 className="text-2xl font-semibold">Related Mentors:</h3>
+              <div className="flex justify-start flex-wrap gap-2 sm:gap-3">
+                {Array.from({ length: 5 }).map((_, index) => (
+                  <MentorCard
+                    className="w-[150px] h-[200px] sm:w-[250px] sm:h-[300px]"
+                    key={index}
+                    name="Khalid Ahmed"
+                    title="UI/UX Designer"
+                    rating={4.5}
+                    image={mentorImage}
+                    id={`${index}`}
+                  />
+                ))}
+              </div>
+            </section>
+          </main>
         </main>
-      </main>
+      )}
     </>
   );
 }
