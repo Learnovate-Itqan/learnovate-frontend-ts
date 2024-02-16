afb3da5e14aba2d0fe22df85d99c4989c3ac5783
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-16 15:52:08 +0200

    Add HeaderSkelton component to TrackHeader

--- src/pages/track/TrackHeader.tsx
+++ src/pages/track/TrackHeader.tsx
@@ -5,6 +5,7 @@ import { Tag } from "@/components/ui/Tag";
 import { Progress } from "@/components/ui/progress";
 
 import TrackHeaderImage from "../../assets/trackHeaderImage.jpg";
+import { HeaderSkelton } from "./HeaderSkelton";
 
 type TrackHeaderProps = {
   track: {
@@ -24,23 +25,27 @@ export function TrackHeader({ track }: TrackHeaderProps) {
         style={{ backgroundImage: `url(${TrackHeaderImage})` }}
         className=" absolute inset-0 -z-10 bg-no-repeat bg-right bg-dark-navy bg-cover  after:content-[''] after:top-0 after:left-0 after:absolute after:-z-1 after:w-full  after:h-[20%]  after:bg-gradient-to-b after:from-dark-navy after:via-dark-navy/20"
       />
-      <main className="container flex flex-col gap-4 justify-center items-start z-20">
-        <h1 className="text-5xl font-semibold">{track?.name}</h1>
-        <p className="text-lg mt-4">{track?.about}</p>
-        <div className="flex gap-5">
-          <Tag className="bg-[#374573] select-none text-white">
-            <HiStar className="text-yellow-500" size={18} />
-            {track?.rating.toFixed(1)}
-          </Tag>
-          <Tag className="bg-[#374573] select-none flex gap-2 text-white">
-            <FaUser className="text-royal-blue" /> {track?.numLearners}+ learners
-          </Tag>
-        </div>
-        <div className="w-full flex flex-col gap-2">
-          <h2 className="ml-1 text-xl font-semibold">{Math.ceil((1 - track.progress) * 100)}% to complete</h2>
-          <Progress className="max-w-96 h-2 bg-dark-navy text-white shadow-2xl" value={track.progress * 100} />
-        </div>
-      </main>
+      {track ? (
+        <main className="container flex flex-col gap-4 justify-center items-start z-20">
+          <h1 className="text-5xl font-semibold">{track?.name}</h1>
+          <p className="text-lg mt-4">{track?.about}</p>
+          <div className="flex gap-5">
+            <Tag className="bg-[#374573] select-none text-white">
+              <HiStar className="text-yellow-500" size={18} />
+              {track?.rating.toFixed(1)}
+            </Tag>
+            <Tag className="bg-[#374573] select-none flex gap-2 text-white">
+              <FaUser className="text-royal-blue" /> {track?.numLearners}+ learners
+            </Tag>
+          </div>
+          <div className="w-full flex flex-col gap-2">
+            <h2 className="ml-1 text-xl font-semibold">{Math.ceil((1 - track?.progress) * 100)}% to complete</h2>
+            <Progress className="max-w-96 h-2 bg-dark-navy text-white shadow-2xl" value={track?.progress * 100} />
+          </div>
+        </main>
+      ) : (
+        <HeaderSkelton />
+      )}
     </header>
   );
 }
