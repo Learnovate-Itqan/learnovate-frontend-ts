d915225a604490d20fb853574d2dabab306c05e3
Mu-selim <muhamadselim70@gmail.com>
2024-02-21 19:08:56 +0200

    Refactor aiChatSlice action names

--- src/redux/slices/aiChatSlice.ts
+++ src/redux/slices/aiChatSlice.ts
@@ -14,26 +14,27 @@ export const aiChatSlice = createSlice({
   name: "aiChat",
   initialState,
   reducers: {
-    setMessages: (state, action: { payload: z.infer<typeof aiChatSchema> }) => {
+    setAIMessages: (state, action: { payload: z.infer<typeof aiChatSchema> }) => {
       return { ...state, chat: [...state.chat, action.payload] };
     },
-    setLoadMessages: (state, action: { payload: z.infer<typeof aiChatSchema>[] }) => {
+    setAILoadMessages: (state, action: { payload: z.infer<typeof aiChatSchema>[] }) => {
       return { ...state, chat: action.payload };
     },
-    setChatSelect: (state, action: { payload: boolean }) => {
+    setAIChatSelect: (state, action: { payload: boolean }) => {
       return { ...state, select: action.payload };
     },
-    setTyping: (state, action: { payload: boolean }) => {
+    setAITyping: (state, action: { payload: boolean }) => {
       return { ...state, typing: action.payload };
     },
-    setError: (state, action: { payload: string | undefined }) => {
+    setAIError: (state, action: { payload: string | undefined }) => {
       return { ...state, error: action.payload };
     },
-    setNewChat: () => {
+    setNewAIChat: () => {
       return { ...initialState };
     },
   },
 });
 
-export const { setMessages, setLoadMessages, setChatSelect, setTyping, setError, setNewChat } = aiChatSlice.actions;
+export const { setAIMessages, setAILoadMessages, setAIChatSelect, setAITyping, setAIError, setNewAIChat } =
+  aiChatSlice.actions;
 export const aiChatReducer = aiChatSlice.reducer;
