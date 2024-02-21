1c36a1f680061a6af5faa432d1f2fe67dd670d53
Mu-selim <muhamadselim70@gmail.com>
2024-02-21 21:25:06 +0200

    Refactor mentor chat layout

--- src/pages/mentor/chat/index.tsx
+++ src/pages/mentor/chat/index.tsx
@@ -2,7 +2,6 @@ import { Navbar } from "@/layouts/Navbar";
 
 import { ChatContainer } from "./components/chatContainer.js";
 import { ChatList } from "./components/chatList";
-import { EmptyChat } from "./components/emptyChat.js";
 import { Search } from "./components/search";
 
 export const MentorChatPage = () => {
@@ -11,12 +10,11 @@ export const MentorChatPage = () => {
       <header>
         <Navbar />
       </header>
-      <main className="flex h-[calc(100vh-4.6rem)] lg:h-[calc(100vh-4.6rem)]">
-        <aside className="bg-dark-navy w-[24rem] min-w-max py-2.5 px-4 space-y-4">
+      <main className="flex h-[calc(100vh-4.6rem)] lg:h-[calc(100vh-4.6rem)] relative">
+        <aside className="bg-dark-navy w-full md:w-[30rem] min-w-max py-2.5 px-4 space-y-4">
           <Search />
           <ChatList />
         </aside>
-        <EmptyChat />
         <ChatContainer />
       </main>
     </>
