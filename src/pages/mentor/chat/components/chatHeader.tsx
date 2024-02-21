775800540b5f8e741397db617e7ec07913801dd3
Mu-selim <muhamadselim70@gmail.com>
2024-02-21 18:04:08 +0200

    Refactor chatHeader component to use UserAvatar

--- src/pages/mentor/chat/components/chatHeader.tsx
+++ src/pages/mentor/chat/components/chatHeader.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { UserAvatar } from "./userAvatar";
 
 type TChatHeader = {
   title: string;
@@ -9,10 +9,7 @@ export const ChatHeader = ({ title, image }: TChatHeader) => {
   return (
     <header className="flex items-center justify-between gap-4 h-16 w-full bg-dark-navy text-white px-4 py-3">
       <div className="flex items-center gap-2 select-none" title={title}>
-        <Avatar className="bg-[#222C54] w-10 h-10">
-          <AvatarImage src={image} alt={title} title={title} />
-          <AvatarFallback>{title.slice(0, 2).toUpperCase()}</AvatarFallback>
-        </Avatar>
+        <UserAvatar name={title} image={image} className="bg-[#222C54] w-10 h-10" />
         <h2 className="text-xl font-bold">{title}</h2>
       </div>
       <div className="relative">
