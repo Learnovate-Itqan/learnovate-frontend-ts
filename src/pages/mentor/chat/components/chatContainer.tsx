e4a7fead40a3e67a6e07ae4097f0755b2aea119e
Mu-selim <muhamadselim70@gmail.com>
2024-02-21 18:03:52 +0200

    Refactor chatContainer component to use AIMessagesBox and add conditional rendering for MessageBox

--- src/pages/mentor/chat/components/chatContainer.tsx
+++ src/pages/mentor/chat/components/chatContainer.tsx
@@ -1,17 +1,18 @@
 import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
-import { v4 } from "uuid";
 
 import learnovateAI from "@/assets/mentors/learnovateAI.webp";
+import { useGetParam } from "@/hooks/useGetParam";
 import { RootState } from "@/redux/store.js";
 
+import { AIMessagesBox } from "./AI/messagesBox";
 import { ChatHeader } from "./chatHeader";
-import { Message } from "./message";
 import { MessageBox } from "./messageBox";
 
 export const ChatContainer = () => {
   const chatContainerRef = useRef<HTMLDivElement>(null);
   const aiChat = useSelector((state: RootState) => state.aiChat);
+  const sourceParam = useGetParam("source");
 
   useEffect(() => {
     if (chatContainerRef.current) {
@@ -19,21 +20,19 @@ export const ChatContainer = () => {
     }
   }, [aiChat.chat]);
 
-  if (!aiChat.select) return null;
+  if (!sourceParam) return null;
+
   return (
-    <section className="overflow-y-auto flex flex-col flex-grow max-h-screen bg-gray-200 border-t border-t-gray-600">
+    <section className="overflow-y-auto flex flex-col flex-grow max-h-screen border-t border-t-gray-600">
       <ChatHeader title="Learnovate AI" image={learnovateAI} />
       <div
         ref={chatContainerRef}
         className="max-h-[calc(100%-8rem)] h-[calc(100%-8rem)] w-full bg-white overflow-y-auto"
       >
-        <div className="w-full p-4 space-y-2">
-          {aiChat.chat.map((message) => (
-            <Message role={message.role} text={message.parts} key={v4()} />
-          ))}
-        </div>
+        <AIMessagesBox />
       </div>
-      <MessageBox />
+      {sourceParam === "ai" && <MessageBox ai />}
+      {sourceParam !== "ai" && <MessageBox sound />}
     </section>
   );
 };
