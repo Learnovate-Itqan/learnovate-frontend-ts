933a767a127c43fdc0274f21b9b76524b2dcb0ad
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-21 13:24:01 +0200

    Update Course page layout and styles to be responsive

--- src/pages/course/index.tsx
+++ src/pages/course/index.tsx
@@ -177,63 +177,22 @@ const mentor = {
 
 export function Course() {
   return (
-    <div className="container space-y-5  py-10">
+    <div className="px-4 sm:container space-y-5 py-10">
       <header>
         <h1 className="text-3xl font-semibold text-dark-navy">{course.title}</h1>
         <p className="text-zinc-500">{mentor.name}</p>
       </header>
       <div className="">
-        <main className="flex gap-6">
-          <section className="grow">
+        <main className=" grid grid-cols-[1fr_20rem] grid-rows-[1fr] gap-6">
+          <section className=" col-span-2 xl:col-span-1">
             <Player src="youtube/neKFZ-Zndkc?si=hJgeMlj0dhGYczsz" className="rounded-xl" />
-            <section className="py-5 grid gap-8">
-              <header className="flex gap-4 flex-wrap ">
-                <Tag className="select-none ">
-                  <HiStar className="text-yellow-500" size={18} />
-                  {course?.rating.toFixed(1)}
-                </Tag>
-                <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
-                  <TbTargetArrow className="text-royal-blue" /> {course?.trackName}
-                </Tag>
-                <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
-                  <IoCalendarNumber className="text-royal-blue" /> {new Date(course?.publishTime).toLocaleDateString()}
-                </Tag>
-                <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
-                  <MdWatchLater className="text-royal-blue" /> {course?.estimatedTime} hrs
-                </Tag>
-                <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
-                  <HiMiniRectangleStack className="text-royal-blue" /> {course?.noChapters} chapters
-                </Tag>
-                <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
-                  <AiFillSignal className="text-royal-blue" /> {course?.cLevel}
-                </Tag>
-                <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
-                  <FaUser className="text-royal-blue" /> {course?.noStudentsEnrolled}+ learners
-                </Tag>
-              </header>
-              <main className="grid gap-2">
-                <h3 className="text-xl font-medium">Description :</h3>
-                <p className=" font-normal text-balance">{course?.description}</p>
-              </main>
-              <footer className="flex flex-wrap gap-4">
-                {course?.keywords.map((topic, index) => (
-                  <Tag
-                    title={topic}
-                    key={index}
-                    className="bg-white capitalize border-royal-blue text-royal-blue border-[1px] font-[400]"
-                  >
-                    {topic}
-                  </Tag>
-                ))}
-              </footer>
-            </section>
           </section>
-          <section className=" grid gap-5">
-            <div className="shadow-custom w-96 rounded-xl overflow-hidden">
+          <section className=" flex h-fit flex-col gap-5 row-span-2 col-span-2 xl:col-span-1">
+            <div className="shadow-custom grow rounded-xl overflow-hidden">
               <header className="px-5 py-8 rounded-xl shadow-custom">
                 <div className="w-full flex flex-col gap-2">
                   <h2 className="ml-1 text-xl font-semibold">{Math.ceil((1 - course?.progress) * 100)}% to complete</h2>
-                  <Progress className="max-w-96 h-2 rounded-full bg-zinc-300" value={course?.progress * 100} />
+                  <Progress className="h-2 rounded-full bg-zinc-300" value={course?.progress * 100} />
                 </div>
               </header>
               <main className="py-5 mb-5 text-dark-navy">
@@ -267,7 +226,7 @@ export function Course() {
                 </Accordion>
               </main>
             </div>
-            <div className="px-5 grid w-96 gap-3 py-5 text-dark-navy shadow-custom rounded-xl">
+            <div className="px-5 grid gap-3 py-5 text-dark-navy shadow-custom rounded-xl">
               <h2 className="text-xl font-[500]">About Instructor</h2>
               <section className="flex gap-3">
                 <UserAvatar className="w-16 h-16" imageUrl={mentorAvatar} name={"mentor"} />
@@ -282,6 +241,47 @@ export function Course() {
                 A passionate and creative front-end developer with a keen eye for design and user experience.
               </p>
             </div>
+          </section>
+          <section className="py-5 flex flex-col col-span-2 xl:col-span-1 gap-8">
+            <header className="flex gap-2 sm:gap-4 flex-wrap ">
+              <Tag className="select-none ">
+                <HiStar className="text-yellow-500" size={18} />
+                {course?.rating.toFixed(1)}
+              </Tag>
+              <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
+                <TbTargetArrow className="text-royal-blue" /> {course?.trackName}
+              </Tag>
+              <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
+                <IoCalendarNumber className="text-royal-blue" /> {new Date(course?.publishTime).toLocaleDateString()}
+              </Tag>
+              <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
+                <MdWatchLater className="text-royal-blue" /> {course?.estimatedTime} hrs
+              </Tag>
+              <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
+                <HiMiniRectangleStack className="text-royal-blue" /> {course?.noChapters} chapters
+              </Tag>
+              <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
+                <AiFillSignal className="text-royal-blue" /> {course?.cLevel}
+              </Tag>
+              <Tag className=" select-none flex gap-1 rounded-xl px-3 ">
+                <FaUser className="text-royal-blue" /> {course?.noStudentsEnrolled}+ learners
+              </Tag>
+            </header>
+            <main className="grid gap-2">
+              <h3 className="text-xl font-medium">Description :</h3>
+              <p className=" font-normal text-balance">{course?.description}</p>
+            </main>
+            <footer className="flex flex-wrap gap-2 sm:gap-4">
+              {course?.keywords.map((topic, index) => (
+                <Tag
+                  title={topic}
+                  key={index}
+                  className="bg-white capitalize border-royal-blue text-royal-blue border-[1px] font-[400]"
+                >
+                  {topic}
+                </Tag>
+              ))}
+            </footer>
           </section>
         </main>
       </div>
