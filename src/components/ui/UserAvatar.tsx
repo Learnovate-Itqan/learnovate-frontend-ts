4f45e5af072d93f6fe1b22d90bd6d0baa6cecf1a
kareem172 <kareemkhalaf1722@gmail.com>
2024-02-21 10:07:20 +0200

    Add className prop to UserAvatar component

--- src/components/ui/UserAvatar.tsx
+++ src/components/ui/UserAvatar.tsx
@@ -1,8 +1,8 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export function UserAvatar({ imageUrl, name }: { imageUrl: string; name: string }) {
+export function UserAvatar({ imageUrl, name, className = "" }: { imageUrl: string; name: string; className?: string }) {
   return (
-    <Avatar>
+    <Avatar className={className}>
       <AvatarImage src={imageUrl} title={name} alt={name} />
       <AvatarFallback className=" bg-royal-blue text-lg">{name?.slice(0, 2).toUpperCase()}</AvatarFallback>
     </Avatar>
