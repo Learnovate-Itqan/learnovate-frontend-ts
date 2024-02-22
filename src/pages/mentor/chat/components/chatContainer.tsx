6f769aba452c7b871ddb5c1025adede755062286
Mu-selim <muhamadselim70@gmail.com>
2024-02-22 23:47:26 +0200

    Update chatContainer styling to fix overflow issue

--- src/pages/mentor/chat/components/chatContainer.tsx
+++ src/pages/mentor/chat/components/chatContainer.tsx
@@ -11,7 +11,7 @@ export const ChatContainer = () => {
   return (
     <div
       className={clsx(
-        "absolute md:static top-0 w-full h-full flex flex-col flex-grow max-h-screen border-t border-t-gray-600 transition-all duration-200 ease-cubic",
+        "absolute md:static top-0 w-full overflow-hidden h-full flex flex-col flex-grow max-h-screen border-t border-t-gray-600 transition-all duration-200 ease-cubic",
         {
           "-left-full": !sourceParam,
           "left-0": sourceParam,
