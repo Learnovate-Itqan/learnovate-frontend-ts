cf43a8342afbc456ad80902913c27fca29ebcd50
Mu-selim <muhamadselim70@gmail.com>
2024-02-22 18:57:32 +0200

    Add chatList module with initial data

---
+++ src/db/chatList.tsx
@@ -0,0 +1,61 @@
+import { v4 as uuid } from "uuid";
+
+import abdelrahmanAwad from "@/assets/mentors/Abdelrahman-Awad.webp";
+import amrMostafa from "@/assets/mentors/Amr-Mostafa.webp";
+import muhammadEmara from "@/assets/mentors/Muhammad-Emara.webp";
+import muhammadIbrahim from "@/assets/mentors/Muhammad-Ibrahim.webp";
+import muhammadSelim from "@/assets/mentors/Muhammad-Selim.webp";
+
+export type TChat = {
+  id: string;
+  name: string;
+  message: string;
+  time: Date;
+  image: string;
+  notRead?: number;
+};
+
+export const CHAT_LIST: TChat[] = [
+  {
+    id: uuid(),
+    name: "Abdelrahman Awad",
+    message:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    time: new Date(),
+    image: abdelrahmanAwad,
+  },
+  {
+    id: uuid(),
+    name: "Amr Mostafa",
+    message:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    time: new Date(),
+    image: amrMostafa,
+  },
+  {
+    id: uuid(),
+    name: "Muhammad Emara",
+    message:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    time: new Date(),
+    image: muhammadEmara,
+    notRead: 99,
+  },
+  {
+    id: uuid(),
+    name: "Muhammad Ibrahim",
+    message:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    time: new Date(),
+    image: muhammadIbrahim,
+    notRead: 8,
+  },
+  {
+    id: uuid(),
+    name: "Muhammad Selim",
+    message:
+      "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    time: new Date(),
+    image: muhammadSelim,
+  },
+];
