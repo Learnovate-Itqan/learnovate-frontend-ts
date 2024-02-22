048b0d6d7027fe2027b5b8171134a9e279dec5c3
Mu-selim <muhamadselim70@gmail.com>
2024-02-22 23:47:31 +0200

    Refactor message component styling

--- src/pages/mentor/chat/components/message.tsx
+++ src/pages/mentor/chat/components/message.tsx
@@ -25,13 +25,13 @@ export const Message = ({ role, text, image, name, time }: TMessage) => {
     >
       {!isUser && <UserAvatar className="bg-[#222C54] w-8 h-8 xs:w-10 xs:h-10" name={name} image={image} />}
       <div
-        className={clsx("max-w-3xl flex flex-col py-1.5 px-2.5 relative", {
+        className={clsx("overflow-hidden lg:max-w-3xl flex flex-col py-1.5 px-2.5 relative", {
           "bg-dark-navy text-white rounded-s-3xl rounded-t-3xl ms-12": isUser,
           "bg-gray-200 text-dark-navy rounded-e-3xl rounded-t-3xl": !isUser,
           "py-2 px-4": time,
         })}
       >
-        <span>{role === "model" ? <RenderMarkDown content={text} /> : <pre>{text.trim()}</pre>}</span>
+        <span>{role === "model" ? <RenderMarkDown content={text} /> : <pre className="whitespace-pre-wrap">{text.trim()}</pre>}</span>
         {time && <span className="text-[0.7rem] mt-1">{DateToAMAndPM(time)}</span>}
       </div>
     </div>
