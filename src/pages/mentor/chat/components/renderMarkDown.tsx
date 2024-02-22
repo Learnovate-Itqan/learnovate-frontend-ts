a5687e54477b9be6f56deab20b051b0ff9f4088a
Mu-selim <muhamadselim70@gmail.com>
2024-02-22 23:36:59 +0200

    Add syntax highlighting to Markdown rendering component

--- src/pages/mentor/chat/components/renderMarkDown.tsx
+++ src/pages/mentor/chat/components/renderMarkDown.tsx
@@ -1,5 +1,7 @@
 import { FC, ReactNode } from "react";
 import Markdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 interface RenderMarkdownProps {
   content: string;
@@ -7,13 +9,21 @@ interface RenderMarkdownProps {
 
 interface ComponentProps {
   children: ReactNode;
+  href?: string;
 }
 
 export const RenderMarkDown: FC<RenderMarkdownProps> = ({ content }) => {
   const P: FC<ComponentProps> = ({ children }) => <p className="text-dark-navy">{children}</p>;
   const Header: FC<ComponentProps> = ({ children }) => <h3 className="text-dark-navy font-bold text-lg">{children}</h3>;
-  const Link: FC<ComponentProps> = ({ children }) => (
-    <a className="text-royal-blue cursor-pointer hover:underline underline-offset-2">{children}</a>
+  const Link: FC<ComponentProps> = ({ children, href }) => (
+    <a
+      target="_blank"
+      rel="noopener noreferrer"
+      href={href}
+      className="text-royal-blue cursor-pointer hover:underline underline-offset-2"
+    >
+      {children}
+    </a>
   );
   return (
     <Markdown
@@ -39,8 +49,35 @@ export const RenderMarkDown: FC<RenderMarkdownProps> = ({ content }) => {
         h6({ children, ...props }) {
           return <Header {...props}>{children}</Header>;
         },
-        a({ children, ...props }) {
-          return <Link {...props}>{children}</Link>;
+        a({ children, href }) {
+          return <Link href={href}>{children}</Link>;
+        },
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <div className="*:no-scrollbar">
+              <div className="w-full bg-gray-500 mt-2 rounded-t-xl text-white px-4 py-1 font-semibold">{match[1]}</div>
+              <SyntaxHighlighter
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-expect-error
+                style={coldarkDark}
+                customStyle={{ borderEndEndRadius: "0.75rem", borderEndStartRadius: "0.75rem", marginTop: 0 }}
+                language={match[1]}
+                PreTag="div"
+                wrapLongLines
+                wrapLines
+                children={String(children).replace(/\n$/, "")}
+                {...props}
+              />
+            </div>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
         },
       }}
     >
