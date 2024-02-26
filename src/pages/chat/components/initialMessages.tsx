8ac8f24566df7f09d23bfc142c28499547a2f37e
Mu-selim <muhamadselim70@gmail.com>
2024-02-27 01:05:39 +0200

    Add InitialMessages component

---
+++ src/pages/chat/components/initialMessages.tsx
@@ -0,0 +1,34 @@
+import { useSearchParams } from "react-router-dom";
+import { initialReservedMessage } from "@/db/chat";
+import { useGetParam } from "@/hooks/useGetParam";
+
+type TInitialMessages = {
+  title: string;
+  description: string;
+  message: string;
+};
+
+export const InitialMessages = ({ title, description, message }: TInitialMessages) => {
+  const id = useGetParam("id");
+  const [, setSearchParams] = useSearchParams();
+
+  const handleClick = async () => {
+    if (id) return;
+    const chatID = await initialReservedMessage(title, message);
+    if (chatID) {
+      console.log(chatID);
+      setSearchParams({ id: chatID });
+    }
+  };
+
+  return (
+    <button
+      type="button"
+      className="select-none text-start flex flex-col border border-royal-blue/40 transition-colors duration-100 ease-cubic hover:bg-white/5 text-white rounded-xl py-2 px-4 w-full h-full"
+      onClick={handleClick}
+    >
+      <span className="text-sm font-semibold">{title}</span>
+      <span className="text-sm text-gray-400 max-w-xs text-balance">{description}</span>
+    </button>
+  );
+};
