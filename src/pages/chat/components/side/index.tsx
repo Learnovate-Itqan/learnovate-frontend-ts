1c3583f11b2732ffe1a3b4f6333eb05edf620ac8
Mu-selim <muhamadselim70@gmail.com>
2024-02-27 18:44:41 +0200

    Update sidebar styling

--- src/pages/chat/components/side/index.tsx
+++ src/pages/chat/components/side/index.tsx
@@ -3,7 +3,7 @@ import { MessagesList } from "./messagesList";
 
 export const SideBar = () => {
   return (
-    <aside className="h-svh overflow-y-hidden w-64 bg-[#0c1028] ps-2 py-4 space-y-6">
+    <aside className="h-svh overflow-y-hidden min-w-64 bg-[#0c1028] ps-2 py-4 space-y-6">
       <SideHeader />
       <MessagesList />
     </aside>
