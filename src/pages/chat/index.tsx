7c31e2d667b2c6dc6e16c8a5289ca543a83fc8c8
Mu-selim <muhamadselim70@gmail.com>
2024-02-27 13:00:01 +0200

    Refactor chat page components

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,30 +1,15 @@
-import { LearnovateLogo } from "@/components/icons/Logo";
-import { getFourRandomExamples } from "@/db/chatExamples";
-
-import { InitialMessages } from "./components/initialMessages";
 import { MessageBox } from "./components/messageBox";
+import { MessagesContainer } from "./components/messages";
+import { NewChat } from "./components/newChat";
 import { SideBar } from "./components/side";
-
-const CHAT_EXAMPLES = getFourRandomExamples();
 
 export const ChatPage = () => {
   return (
     <div className="bg-dark-navy h-svh w-full flex">
       <SideBar />
       <div className="flex-grow h-full overflow-hidden flex flex-col">
-        <div className="flex-grow flex flex-col items-center justify-end py-2 gap-4 px-4 lg:px-8">
-          <div className="flex-grow flex flex-col items-center justify-center py-8">
-            <span className="p-2 bg-white w-fit rounded-full" title="Learnovate">
-              <LearnovateLogo className="h-8 w-8" />
-            </span>
-            <h1 className="text-white text-2xl font-bold mt-4">How can I help you today?</h1>
-          </div>
-          <div className="w-full h-auto max-w-3xl grid grid-cols-2 gap-y-2 gap-x-4">
-            {CHAT_EXAMPLES.map((example, idx) => (
-              <InitialMessages key={example.title} {...example} duration={(idx + 4) * 100} />
-            ))}
-          </div>
-        </div>
+        <NewChat />
+        <MessagesContainer />
         <MessageBox />
       </div>
     </div>
