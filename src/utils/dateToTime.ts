6a2bec699d9d644d39f1e71d694ee35b1570d242
Mu-selim <muhamadselim70@gmail.com>
2024-02-27 16:17:57 +0200

    Remove unused dateToTime utility function

--- src/utils/dateToTime.ts
+++
@@ -1,9 +0,0 @@
-export const DateToAMAndPM = (date: number | string) => {
-  const dateObj = new Date(date);
-  const time = Intl.DateTimeFormat("en-US", {
-    hour: "numeric",
-    minute: "numeric",
-    hour12: true,
-  }).format(dateObj);
-  return time;
-};
