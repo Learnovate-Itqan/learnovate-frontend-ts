9f57df9d409a926ea8d78f435454ff54b4d5d985
Mu-selim <muhamadselim70@gmail.com>
2024-02-27 13:00:05 +0200

    Add newChat component to handle initial messages in chat

---
+++ src/pages/chat/components/newChat.tsx
@@ -0,0 +1,33 @@
+import { useLiveQuery } from "dexie-react-hooks";
+
+import { LearnovateLogo } from "@/components/icons/Logo";
+import { getChatByID } from "@/db/chat";
+import { getFourRandomExamples } from "@/db/chatExamples";
+import { useGetParam } from "@/hooks/useParamHelpers";
+
+import { InitialMessages } from "./initialMessages";
+
+const CHAT_EXAMPLES = getFourRandomExamples();
+
+export const NewChat = () => {
+  const idParam = useGetParam("id");
+  const chat = useLiveQuery(() => getChatByID(idParam), [idParam]);
+
+  if (chat && idParam) return null;
+
+  return (
+    <div className="flex-grow flex flex-col items-center justify-end py-2 gap-4 px-4 lg:px-8">
+      <div className="flex-grow flex flex-col items-center justify-center py-8">
+        <span className="p-2 bg-white w-fit rounded-full" title="Learnovate">
+          <LearnovateLogo className="h-8 w-8" />
+        </span>
+        <h1 className="text-white text-2xl font-bold mt-4">How can I help you today?</h1>
+      </div>
+      <div className="w-full h-auto max-w-3xl grid grid-cols-2 gap-y-2 gap-x-4">
+        {CHAT_EXAMPLES.map((example, idx) => (
+          <InitialMessages key={example.title} {...example} duration={(idx + 4) * 100} />
+        ))}
+      </div>
+    </div>
+  );
+};
