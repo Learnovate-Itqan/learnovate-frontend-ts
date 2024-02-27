ce66aece4ace2094db11452c1abc3d59f93b4490
Mu-selim <muhamadselim70@gmail.com>
2024-02-27 17:32:59 +0200

    Update chat ID field and add auto-incrementing ID for chat table

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import Dexie, { Table } from "dexie";
 import { z } from "zod";
 
 export const Chat = z.object({
-  id: z.string(),
+  chatID: z.string(),
   role: z.union([z.literal("user"), z.literal("model")]),
   parts: z.string(),
   time: z.number(),
@@ -21,7 +21,7 @@ export class DexieDB extends Dexie {
   constructor() {
     super("Learnovate");
     this.version(1).stores({
-      chat: "id, role, parts, time",
+      chat: "++id, chatID, role, parts, time",
       chatTitle: "id, title, time",
     });
   }
