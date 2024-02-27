86a5dd8aa922b66c4be7a35575f91b2e3577f825
Mu-selim <muhamadselim70@gmail.com>
2024-02-27 19:48:55 +0200

    Refactor useHandleAIChat hook to improve code structure and error handling

--- src/hooks/useHandleAIChat.tsx
+++ src/hooks/useHandleAIChat.tsx
@@ -1,63 +1,70 @@
-import { useEffect } from "react";
 import { UseFormReturn } from "react-hook-form";
+import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
+import { v4 as uuid } from "uuid";
 import { z } from "zod";
 
-import { startChat } from "@/lib/aiChat";
-import { setAIError, setAILoadMessages, setAIMessages, setAITyping, setNewAIChat } from "@/redux/slices/aiChatSlice";
-import { aiChatSchema } from "@/schemas/aiChat";
+import { db } from "@/db";
+import { deleteChat, sendMessage } from "@/db/chat";
+import { generateTitle, startChat } from "@/lib/aiChat";
+import { setAssistantTyping } from "@/redux/slices/aiChatSlice";
+import { messageBoxSchema } from "@/schemas/messageBox";
 
-type TForm =
-  | UseFormReturn<
-      {
-        text: string;
-        image?: string | undefined;
-      },
-      unknown,
-      undefined
-    >
-  | undefined;
+import { useChatExist } from "./useChatExist";
+import { useClearParam, useGetParam, useSetParam } from "./useParamHelpers";
 
-type TMessage = z.infer<typeof aiChatSchema>;
+type TForm = UseFormReturn<z.infer<typeof messageBoxSchema>>;
 
 export const useHandleAIChat = (form?: TForm) => {
   const dispatch = useDispatch();
-  const aiChat = startChat();
+  const idParam = useGetParam("id");
+  const clearIDParam = useClearParam();
+  const existingChat = useChatExist();
+  const setParam = useSetParam();
+  const model = startChat(existingChat || []);
+  const chatTitle = generateTitle();
 
-  const aiChatHandler = {
-    mutateMessage: async (message: string) => {
-      if (!form) return;
-      const userMessage: TMessage = { role: "user", parts: message, time: Date.now() };
-      dispatch(setAIMessages(userMessage));
-      form.reset();
-      dispatch(setAITyping(true));
+  const handleMessage = {
+    startChat: async (message: string) => {
+      const id = uuid();
+      const time = Date.now();
       try {
-        const result = await aiChat.sendMessage(message);
+        setParam({ param: "id", value: id });
+        await db.chat.add({ chatID: id, role: "user", parts: message, time });
+        form?.reset();
+        await db.chatTitle.add({ id, title: await chatTitle(message), time });
+        dispatch(setAssistantTyping(true));
+        const result = await model.sendMessage(message);
         const response = result.response;
         const text = response.text();
-        dispatch(setAITyping(false));
-        const aiMessage: TMessage = { role: "model", parts: text, time: Date.now() };
-        dispatch(setAIMessages(aiMessage));
-        const history: z.infer<typeof aiChatSchema>[] = JSON.parse(sessionStorage.getItem("ai") || "[]");
-        sessionStorage.setItem("ai", JSON.stringify([...history, userMessage, aiMessage]));
+        dispatch(setAssistantTyping(false));
+        await sendMessage(id, "model", text);
+        return id;
       } catch (error) {
-        const timer = setTimeout(() => {
-          dispatch(setAITyping(false));
-          dispatch(setAIError("Something went wrong, please try again later"));
-        }, 1000);
-        return () => clearTimeout(timer);
+        toast.error("Sorry, Something went wrong. Please try again.");
+        dispatch(setAssistantTyping(false));
+        clearIDParam("id");
+        deleteChat(id);
       }
     },
-    clear: () => {
-      dispatch(setNewAIChat());
-      sessionStorage.removeItem("ai");
+    continueChat: async (message: string) => {
+      if (!idParam) return;
+      try {
+        await sendMessage(idParam, "user", message);
+        form?.reset();
+        dispatch(setAssistantTyping(true));
+        const result = await model.sendMessage(message);
+        const response = result.response;
+        const text = response.text();
+        dispatch(setAssistantTyping(false));
+        await sendMessage(idParam, "model", text);
+      } catch (error) {
+        toast.error("Sorry, Something went wrong. Please try again.");
+        dispatch(setAssistantTyping(false));
+        clearIDParam("id");
+        deleteChat(idParam);
+      }
     },
   };
-
-  useEffect(() => {
-    const history: z.infer<typeof aiChatSchema>[] = JSON.parse(sessionStorage.getItem("ai") || "[]");
-    dispatch(setAILoadMessages(history));
-  }, [dispatch]);
-
-  return aiChatHandler;
+  return handleMessage;
 };
