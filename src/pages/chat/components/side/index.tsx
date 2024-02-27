1574a9826e3836b862ff60786d31ab2f0309485d
Mu-selim <muhamadselim70@gmail.com>
2024-02-27 02:25:14 +0200

    Add MessagesList component to SideBar

--- src/pages/chat/components/side/index.tsx
+++ src/pages/chat/components/side/index.tsx
@@ -1,9 +1,11 @@
 import { SideHeader } from "./header";
+import { MessagesList } from "./messagesList";
 
 export const SideBar = () => {
   return (
-    <aside className="h-svh overflow-y-hidden w-64 bg-[#0c1028] p-4">
+    <aside className="h-svh overflow-y-hidden w-64 bg-[#0c1028] ps-2 py-4 space-y-6">
       <SideHeader />
+      <MessagesList />
     </aside>
   );
 };
