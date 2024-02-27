a53898bbeea83181cfe7359b3b2555ba1df7e83e
Mu-selim <muhamadselim70@gmail.com>
2024-02-27 19:01:45 +0200

    Fix formatting in MessagesContainer component

--- src/pages/chat/components/messages/index.tsx
+++ src/pages/chat/components/messages/index.tsx
@@ -8,7 +8,7 @@ export const MessagesContainer = () => {
   if (!chatExist) return null;
 
   return (
-    <ul className=" flex-grow w-full pt-8 space-y-4 pe-2 chat-scrollbar text-white max-h-[calc(100svh-6rem)] overflow-y-auto">
+    <ul className="flex-grow w-full pt-8 space-y-4 pe-2 chat-scrollbar text-white max-h-[calc(100svh-6rem)] overflow-y-auto">
       {chatExist.map((message) => (
         <Message key={message.time} role={message.role} text={message.parts} />
       ))}
