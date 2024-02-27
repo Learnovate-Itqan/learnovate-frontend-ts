da821211a4b27147b999736d2c14492ddf4cc477
Mu-selim <muhamadselim70@gmail.com>
2024-02-27 18:44:48 +0200

    Add RenderMarkDown component to Message component

--- src/pages/chat/components/messages/message.tsx
+++ src/pages/chat/components/messages/message.tsx
@@ -1,6 +1,7 @@
 import learnovateAI from "@/assets/learnovateAI.webp";
 
 import { UserAvatar } from "../userAvatar";
+import { RenderMarkDown } from "./renderMarkDown";
 
 type TMessage = {
   role: "user" | "model";
@@ -13,13 +14,17 @@ export const Message = ({ role, text, image }: TMessage) => {
   const name = isModel ? "Learnovate Assistant" : "You";
 
   return (
-    <li className="flex flex-col gap-2">
-      <div className="flex items-center gap-2 select-none">
-        <UserAvatar image={isModel ? learnovateAI : image} name={name} className="w-8 h-8" />
-        <span className="text-lg font-bold">{name}</span>
-      </div>
-      <div className="flex flex-col gap-1">
-        <span>{text}</span>
+    <li className="flex flex-col items-center gap-2 w-full">
+      <div className="w-full max-w-3xl">
+        <div className="flex items-center gap-2 select-none">
+          <UserAvatar image={isModel ? learnovateAI : image} name={name} className="w-8 h-8" />
+          <span className="text-lg font-bold">{name}</span>
+        </div>
+        <div className="flex flex-col gap-1 ps-10">
+          <span>
+            {isModel ? <RenderMarkDown content={text} /> : <pre className="whitespace-pre-wrap">{text.trim()}</pre>}
+          </span>
+        </div>
       </div>
     </li>
   );
