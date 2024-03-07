1f6d43e61345e3e590a55efd68d42b52532c290b
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-07 03:55:27 +0200

    add socket listener to get all te user already exist in the room

--- src/contexts/RoomContext.tsx
+++ src/contexts/RoomContext.tsx
@@ -1,20 +1,48 @@
 import Peer from "peerjs";
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
 
 import { usePeerConnection } from "@/hooks/usePeerConnection";
+import { addAllPeers, removePeer } from "@/reducers/peersActions";
+import { PeerState, peersReducer } from "@/reducers/peersReducer";
+import { socket } from "@/socket";
 
 type RoomContextValues = {
   myPeer: Peer;
   myStream: MediaStream;
+  peers: PeerState;
 };
 const RoomContext = createContext<RoomContextValues>({
   myPeer: new Peer(),
   myStream: new MediaStream(),
+  peers: {},
 });
 export default function RoomProvider({ children }: { children: React.ReactNode }) {
   const { myPeer, myStream } = usePeerConnection();
+  const [peers, peersDispatcher] = useReducer(peersReducer, {});
+
+  useEffect(() => {
+    // add listener to get-users after joining room
+    function getUsers({ users }: { users: PeerState }) {
+      peersDispatcher(addAllPeers(users));
+    }
+    socket.on("get-users", getUsers);
+
+    // add listener to remove disconnected user
+    function removeDisconnectedUser({ userId }: { userId: string }) {
+      console.log("user disconnected", userId);
+      peersDispatcher(removePeer(userId));
+    }
+    socket.on("user-disconnected", removeDisconnectedUser);
+
+    // cleanup
+    return () => {
+      socket.off("get-users");
+      socket.off("user-disconnected");
+    };
+  }, []);
+
   return (
-    <RoomContext.Provider value={{ myPeer: myPeer || new Peer(), myStream: myStream || new MediaStream() }}>
+    <RoomContext.Provider value={{ myPeer: myPeer || new Peer(), myStream: myStream || new MediaStream(), peers }}>
       {children}
     </RoomContext.Provider>
   );
