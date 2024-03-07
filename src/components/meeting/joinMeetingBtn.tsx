9325f3374441e4dfc06a0f45a525fe8c5c5aeab8
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-07 16:55:25 +0200

    Fix import and add user authentication check in JoinMeetingBtn component

--- src/components/meeting/joinMeetingBtn.tsx
+++ src/components/meeting/joinMeetingBtn.tsx
@@ -1,17 +1,14 @@
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
-
-// import { RootState } from "@/redux/store";
-// import { socket } from "@/socket";
+import { RootState } from "@/redux/store";
 
 export function JoinMeetingBtn({ roomId }: { roomId: string }) {
   const navigate = useNavigate();
-  // const user = useSelector((state: RootState) => state.auth);
+  const user = useSelector((state: RootState) => state.auth);
   function joinMeeting() {
-    // if (!user.authStatus) return;
-    // socket.emit("join-room", { roomId, userId: user.id, userName: user.name });
+    if (!user.authStatus) return;
     navigate(`/meeting/${roomId}`);
   }
   return <Button onClick={joinMeeting}>Join Meeting</Button>;
