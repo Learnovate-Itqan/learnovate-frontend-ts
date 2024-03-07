5ce7abc22cac60f2671b8930a14c39b1f757bdfe
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-07 17:01:55 +0200

    add call answering and calling new joined users

--- src/contexts/RoomContext.tsx
+++ src/contexts/RoomContext.tsx
@@ -1,9 +1,11 @@
 import Peer from "peerjs";
 import React, { createContext, useContext, useEffect, useReducer } from "react";
+import { useSelector } from "react-redux";
 
 import { usePeerConnection } from "@/hooks/usePeerConnection";
-import { addAllPeers, removePeer } from "@/reducers/peersActions";
+import { addAllPeers, addPeerName, addPeerStream, removePeer } from "@/reducers/peersActions";
 import { PeerState, peersReducer } from "@/reducers/peersReducer";
+import { RootState } from "@/redux/store";
 import { socket } from "@/socket";
 
 type RoomContextValues = {
@@ -17,6 +19,7 @@ const RoomContext = createContext<RoomContextValues>({
   peers: {},
 });
 export default function RoomProvider({ children }: { children: React.ReactNode }) {
+  const userName = useSelector((state: RootState) => state.auth.name);
   const { myPeer, myStream } = usePeerConnection();
   const [peers, peersDispatcher] = useReducer(peersReducer, {});
 
@@ -40,6 +43,46 @@ export default function RoomProvider({ children }: { children: React.ReactNode }
       socket.off("user-disconnected");
     };
   }, []);
+
+  useEffect(() => {
+    // if myPeer or myStream is not available return
+    if (!myPeer || !myStream) return;
+
+    // add listener for new user joined
+    socket.on("new-user-joined", ({ userId, userName: name }: { userId: string; userName: string }) => {
+      console.log("new user joined", userId);
+      const call = myPeer.call(userId, myStream, {
+        metadata: { callerName: userName, isSharingScreen: false },
+      });
+      console.log("calling new user", call);
+      // add listener for streams from the new user
+      call.on("stream", (userVideoStream) => {
+        peersDispatcher(addPeerStream(userId, userVideoStream));
+      });
+      // store the new user's name
+      peersDispatcher(addPeerName(userId, name));
+    });
+
+    // add listener for call
+    myPeer.on("call", (call) => {
+      const { callerName } = call.metadata;
+      // answer the call and send my stream
+      console.log("answering call", call);
+      //   store caller's name
+      peersDispatcher(addPeerName(call.peer, callerName));
+      call.answer(myStream);
+      // add listener for streams from the caller peer
+      call.on("stream", (userVideoStream) => {
+        peersDispatcher(addPeerStream(call.peer, userVideoStream));
+      });
+    });
+
+    // cleanup
+    return () => {
+      socket.off("new-user-joined");
+      myPeer.off("call");
+    };
+  }, [myPeer, myStream, userName]);
 
   return (
     <RoomContext.Provider value={{ myPeer: myPeer || new Peer(), myStream: myStream || new MediaStream(), peers }}>
