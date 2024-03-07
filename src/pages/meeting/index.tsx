cfe92a774ef47783f747c96c2b49bdee3b9e00a1
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-07 17:04:53 +0200

    Add user join-room functionality and display all connected peers

--- src/pages/meeting/index.tsx
+++ src/pages/meeting/index.tsx
@@ -1,12 +1,34 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
 import { VideoStreamPlayer } from "@/components/meeting/VideoStreamPlayer";
 import { useRoom } from "@/contexts/RoomContext";
+import { RootState } from "@/redux/store";
+import { socket } from "@/socket";
 
 export function Meeting() {
-  const { myStream, myPeer } = useRoom();
+  const { id: roomId } = useParams();
+  const { myStream, myPeer, peers } = useRoom();
+  const userName = useSelector((state: RootState) => state.auth?.name);
+
+  useEffect(() => {
+    if (!(myPeer.id && myStream && roomId && userName)) return;
+    socket.emit("join-room", { roomId, userId: myPeer.id, userName });
+
+    return () => {
+      socket.off("join-room");
+    };
+  }, [myPeer, myStream, roomId, userName]);
   return (
     <div>
       <VideoStreamPlayer stream={myStream} />
       {myPeer && <p>Peer ID: {myPeer.id}</p>}
+      {Object.keys(peers).map((peerId) => {
+        if (peerId === myPeer.id) return null;
+        if (!peers[peerId].stream) return null;
+        return <VideoStreamPlayer key={peerId} stream={peers[peerId].stream || new MediaStream()} />;
+      })}
     </div>
   );
 }
