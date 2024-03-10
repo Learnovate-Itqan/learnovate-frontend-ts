c9643be5c6dc22d53c2ced1debe0c2245e507c73
Mu-selim <muhamadselim70@gmail.com>
2024-03-10 20:34:25 +0200

    Add CVForm component to MentorEditPage

--- src/pages/mentor/edit/index.tsx
+++ src/pages/mentor/edit/index.tsx
@@ -1,4 +1,5 @@
 import { BasicInfoForm } from "./components/basicInfoForm";
+import { CVFrom } from "./components/cvForm";
 import { Header } from "./components/header";
 import { ImageForm } from "./components/imageForm";
 
@@ -7,11 +8,12 @@ export const MentorEditPage = () => {
     <main className="w-full py-6 space-y-4">
       <Header />
       <div className="container flex gap-4">
-        <div className="basis-1/2">
-          <div className="bg-gray-200 p-4 rounded-lg flex gap-4 shadow-xl">
+        <div className="basis-1/2 space-y-6">
+          <div className="bg-gray-200 p-4 rounded-lg flex gap-4 shadow-lg">
             <ImageForm />
             <BasicInfoForm />
           </div>
+          <CVFrom />
         </div>
         {/* <div className="basis-1/2">csasca</div> */}
       </div>
