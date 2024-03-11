e2d7e84875934ea7aaa822b1539f00c916e05231
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-11 17:55:55 +0200

    Refactor MeetingMember component to display video stream for current user and peers

--- src/components/meeting/MeetingMember.tsx
+++ src/components/meeting/MeetingMember.tsx
@@ -1,17 +1,26 @@
-import { z } from "zod";
+import { useSelector } from "react-redux";
 
-import { userSchema } from "@/schemas/userSchema";
+import { useRoom } from "@/contexts/RoomContext";
+import { RootState } from "@/redux/store";
 
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { VideoStreamPlayer } from "./VideoStreamPlayer";
 
-export function MeetingMember({ member }: { member: z.infer<typeof userSchema> }) {
+export function MeetingMember({ memberId }: { memberId: string }) {
+  const userId = useSelector((state: RootState) => state.auth?.id);
+  const { myStream, peers } = useRoom();
+  if (!memberId) return null;
+  if (memberId === userId) return <VideoStreamPlayer stream={myStream} className="" muted autoPlay playsInline />;
+  if (peers[memberId]?.stream) {
+    return <VideoStreamPlayer stream={peers[memberId].stream || new MediaStream()} className="" autoPlay playsInline />;
+  }
   return (
     <div className=" p-10 flex flex-col justify-center items-center gap-3 grow overflow-hidden bg-[#222C54] text-white rounded-md w-full">
       <Avatar className="w-24 h-24">
-        <AvatarImage src={member.image} title={member.name} alt={member.name} />
-        <AvatarFallback className=" bg-royal-blue text-lg">{member.name?.slice(0, 2).toUpperCase()}</AvatarFallback>
+        <AvatarImage src={"https://avatars.githubusercontent.com/u/47269252?v=4"} title={memberId} alt={memberId} />
+        <AvatarFallback className=" bg-royal-blue text-lg">{memberId?.slice(0, 2).toUpperCase()}</AvatarFallback>
       </Avatar>
-      <span>{member.name}</span>
+      <span>{memberId}</span>
     </div>
   );
 }
