d8a80779f8f570b81aa468315032be45e88fc199
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-11 17:56:27 +0200

    Refactor MeetingMembers component to display all active peers

--- src/pages/meeting/MeetingMembers.tsx
+++ src/pages/meeting/MeetingMembers.tsx
@@ -1,13 +1,12 @@
-import { z } from "zod";
-
 import { MeetingMember } from "@/components/meeting/MeetingMember";
-import { userSchema } from "@/schemas/userSchema";
+import { useRoom } from "@/contexts/RoomContext";
 
-export function MeetingMembers({ users }: { users: z.infer<typeof userSchema>[] }) {
+export function MeetingMembers() {
+  const { peers } = useRoom();
   return (
     <div className=" flex flex-col gap-3 grow m-2 mb-0  scrollbar">
-      {users.map((user) => (
-        <MeetingMember key={user.id} member={user as z.infer<typeof userSchema>} />
+      {Object.keys(peers).map((userId) => (
+        <MeetingMember key={userId} memberId={peers[userId].userId} />
       ))}
     </div>
   );
