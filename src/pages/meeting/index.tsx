c7c4f7d11c2f3830cde149e22b5ac78c891c7f11
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-12 17:03:38 +0200

    Refactor meeting page components

--- src/pages/meeting/index.tsx
+++ src/pages/meeting/index.tsx
@@ -1,53 +1,32 @@
-import { useEffect, useState } from "react";
-import { BiSolidVideoOff } from "react-icons/bi";
-import { BsArrowUpSquareFill, BsChatDotsFill } from "react-icons/bs";
-import { FaCompressArrowsAlt, FaExpandArrowsAlt, FaUsers } from "react-icons/fa";
-import { HiSpeakerWave } from "react-icons/hi2";
-import { ImPhoneHangUp } from "react-icons/im";
-import { IoMdSettings } from "react-icons/io";
-import { RiMicOffFill } from "react-icons/ri";
+import { useEffect } from "react";
 import { TiVideo } from "react-icons/ti";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { z } from "zod";
 
 import { VideoStreamPlayer } from "@/components/meeting/VideoStreamPlayer";
 import { useRoom } from "@/contexts/RoomContext";
 import { RootState } from "@/redux/store";
-import { userSchema } from "@/schemas/userSchema";
 import { socket } from "@/socket";
 
-import { MeetingChat } from "./MeetingChat";
-import { MeetingMembers } from "./MeetingMembers";
-
-const USERS = [
-  {
-    id: "5",
-    name: "Kareem",
-    image: "https://avatars.githubusercontent.com/u/47269252?v=4",
-  },
-  {
-    id: "6",
-    name: "Kareem",
-    image: "https://avatars.githubusercontent.com/u/47269252?v=4",
-  },
-];
+import { MeetingControllers } from "./MeetingControllers";
+import { MeetingSidebar } from "./MeetingSidebar";
 
 export function Meeting() {
   const { id: roomId } = useParams();
-  const { myStream, myPeer } = useRoom();
+  const { myStream, myPeer, peers } = useRoom();
   const userName = useSelector((state: RootState) => state.auth?.name);
-  const [asideState, setAsideState] = useState(false);
-  const [chatState, setChatState] = useState(false);
+  const userId = useSelector((state: RootState) => state.auth?.id);
 
   useEffect(() => {
     if (!(myPeer.id && myStream && roomId && userName)) return;
-    socket.emit("join-room", { roomId, userId: myPeer.id, userName });
+    socket.emit("join-room", { roomId, peerId: myPeer.id, userName, userId });
 
     return () => {
       socket.off("join-room");
     };
-  }, [myPeer, myStream, roomId, userName]);
+  }, [myPeer, myStream, roomId, userName, userId]);
+
+  console.log(peers);
   return (
     <section className="flex">
       <main className="relative w-full h-dvh overflow-hidden">
@@ -62,56 +41,9 @@ export function Meeting() {
           <TiVideo className="inline-block mr-2 w-6 h-6 text-royal-blue" />
           <span>Meeting with Kareem</span>
         </h1>
-        <div className=" absolute bottom-10 flex z-20 gap-4 justify-center w-full items-center px-20">
-          <button className="p-3 flex justify-center bg-dark-navy transition-colors rounded-full text-zinc-300 hover:text-zinc-400">
-            <HiSpeakerWave className="w-6 h-6  " />
-          </button>
-          <div className="flex-1"></div>
-
-          <button className="p-3 flex justify-center bg-dark-navy transition-colors rounded-full text-zinc-300 hover:text-zinc-400">
-            <BsArrowUpSquareFill className="w-6 h-6  " />
-          </button>
-          <button className="p-3 flex justify-center bg-dark-navy transition-colors rounded-full text-zinc-300 hover:text-zinc-400">
-            <RiMicOffFill className="w-6 h-6  " />
-          </button>
-          <button className="p-3 flex justify-center bg-red-500 transition-colors rounded-full hover:bg-red-600">
-            <ImPhoneHangUp className="w-10 h-10 text-white" />
-          </button>
-          <button className="p-3 flex justify-center bg-dark-navy transition-colors rounded-full text-zinc-300 hover:text-zinc-400">
-            <BiSolidVideoOff className="w-6 h-6  " />
-          </button>
-          <button className="p-3 flex justify-center bg-dark-navy transition-colors rounded-full text-zinc-300 hover:text-zinc-400">
-            <IoMdSettings className="w-6 h-6  " />
-          </button>
-          <div className="flex-1"></div>
-          <button
-            className="p-3 flex justify-center bg-dark-navy transition-colors rounded-full text-zinc-300 hover:text-zinc-400"
-            onClick={() => setAsideState((prev) => !prev)}
-          >
-            {!asideState ? <FaExpandArrowsAlt className="w-6 h-6  " /> : <FaCompressArrowsAlt className="w-6 h-6  " />}
-          </button>
-        </div>
+        <MeetingControllers />
       </main>
-      <aside
-        className={` w-[450px] h-screen flex flex-col justify-evenly gap-2 bg-dark-navy overflow-hidden transition-[max-width]  ${asideState ? " max-w-full " : " max-w-0"}`}
-      >
-        {chatState ? <MeetingChat /> : <MeetingMembers users={USERS as z.infer<typeof userSchema>[]} />}
-
-        <footer className="bg-[#222c54] grid grid-cols-2 m-2 rounded-md p-1 overflow-hidden">
-          <button
-            className={`p-3 flex justify-center transition-colors rounded-md text-zinc-300 hover:text-zinc-400 ${!chatState && "bg-royal-blue/20"}`}
-            onClick={() => setChatState(false)}
-          >
-            <FaUsers className="w-6 h-6 " />
-          </button>
-          <button
-            className={`p-3 flex justify-center transition-colors rounded-md text-zinc-300 hover:text-zinc-400 ${chatState && "bg-royal-blue/20"}`}
-            onClick={() => setChatState(true)}
-          >
-            <BsChatDotsFill className="w-6 h-6 " />
-          </button>
-        </footer>
-      </aside>
+      <MeetingSidebar />
     </section>
   );
 }
