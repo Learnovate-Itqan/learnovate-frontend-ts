7bc6bd9da4e86bd4061cfcc0ace267a432edcc82
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-12 17:03:03 +0200

    Update MeetingMembers component to add a scrollbar

--- src/pages/meeting/MeetingMembers.tsx
+++ src/pages/meeting/MeetingMembers.tsx
@@ -4,7 +4,7 @@ import { useRoom } from "@/contexts/RoomContext";
 export function MeetingMembers() {
   const { peers } = useRoom();
   return (
-    <div className=" flex flex-col gap-3 grow m-2 mb-0  scrollbar">
+    <div className=" flex flex-col gap-3 grow m-2 mb-0 max-h-[calc(100dvh-90px)] overflow-y-auto overflow-x-hidden scrollbar">
       {Object.keys(peers).map((userId) => (
         <MeetingMember key={userId} memberId={peers[userId].userId} />
       ))}
