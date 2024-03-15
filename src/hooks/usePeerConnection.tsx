2cfb944b3349f4ee73da6afb54eca7873bbf01e0
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-15 16:10:55 +0200

    Refactor usePeerConnection hook to include emit toggling mic and camera to other users

--- src/hooks/usePeerConnection.tsx
+++ src/hooks/usePeerConnection.tsx
@@ -1,22 +1,27 @@
 import Peer from "peerjs";
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import { v4 } from "uuid";
 
+import { RootState } from "@/redux/store";
+import { socket } from "@/socket";
+
 export function usePeerConnection() {
+  const myId = useSelector((state: RootState) => state.auth.id);
+  const { id: roomId } = useParams();
   const [myPeer, setMyPeer] = useState<Peer>();
   const [myStream, setMyStream] = useState<MediaStream>();
-  const [userId, setUserId] = useState<string>();
   const [isCameraEnabled, setIsCameraEnabled] = useState(true);
-  const [isMicEnabled, setIsMicEnabled] = useState(true);
+  const [isMicEnabled, setIsMicEnabled] = useState(false);
   useEffect(() => {
     // create a new peer connection to make calls and answer calls
 
     // generate a random user id to setup peer
     /* when I tried to get the user's id from localStorage or from a state in the user slice the peer connection broke so I generated the userId here and it seemed to be working*/
-    const userId = v4();
-    const peer = new Peer(userId);
+    const peerId = v4();
+    const peer = new Peer(peerId);
     // store the user id and peer connection
-    setUserId(userId);
     setMyPeer(peer);
   }, []);
 
@@ -24,19 +29,25 @@ export function usePeerConnection() {
     // get user media(camera and mic)
     try {
       navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+        stream.getVideoTracks()[0].enabled = true;
+        stream.getAudioTracks()[0].enabled = false;
+
         setMyStream(stream);
       });
     } catch (err) {
       console.error(err);
     }
+    //
   }, []);
 
   function toggleCamera() {
     if (!myStream) return;
     const videoTrack = myStream.getVideoTracks()[0];
     if (videoTrack) {
-      videoTrack.enabled = !isCameraEnabled;
       setIsCameraEnabled(!isCameraEnabled);
+      videoTrack.enabled = !isCameraEnabled;
+      if (!myId || !roomId) return;
+      socket.emit("camera-status", { userId: myId, isCameraEnabled: !isCameraEnabled, roomId });
     }
   }
 
@@ -46,7 +57,9 @@ export function usePeerConnection() {
     if (audioTrack) {
       audioTrack.enabled = !isMicEnabled;
       setIsMicEnabled(!isMicEnabled);
+      if (!myId || !roomId) return;
+      socket.emit("mic-status", { userId: myId, isMicEnabled: !isMicEnabled, roomId });
     }
   }
-  return { myPeer, myStream, userId, toggleCamera, toggleMic, isCameraEnabled, isMicEnabled };
+  return { myPeer, myStream, toggleCamera, toggleMic, isCameraEnabled, isMicEnabled };
 }
