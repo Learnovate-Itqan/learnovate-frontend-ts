00162a3ac64ec9f61398341db1be04bb6a2bfae1
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-15 16:51:28 +0200

    Fix video stream player to handle undefined stream

--- src/components/meeting/VideoStreamPlayer.tsx
+++ src/components/meeting/VideoStreamPlayer.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useRef } from "react";
 
 interface VideoStreamPlayerProps extends React.VideoHTMLAttributes<HTMLVideoElement> {
-  stream: MediaStream;
+  stream: MediaStream | undefined;
 }
 export function VideoStreamPlayer({ stream, ...props }: VideoStreamPlayerProps) {
   const videoRef = useRef<HTMLVideoElement>(null);
   useEffect(() => {
     if (videoRef.current) {
-      videoRef.current.srcObject = stream;
+      videoRef.current.srcObject = stream || null;
     }
   }, [stream]);
   return <video ref={videoRef} {...props}></video>;
