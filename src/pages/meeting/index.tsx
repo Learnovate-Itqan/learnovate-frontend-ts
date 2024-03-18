beac75a7085d349877d54e3be399accfcd250faf
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-18 07:40:31 +0200

    Add isCameraEnabled and isMicEnabled to join-room event

--- src/pages/meeting/index.tsx
+++ src/pages/meeting/index.tsx
@@ -13,17 +13,18 @@ import { MeetingSidebar } from "./MeetingSidebar";
 
 export function Meeting() {
   const { id: roomId } = useParams();
-  const { myStream, myPeer } = useRoom();
+  const { myStream, myPeer, isCameraEnabled, isMicEnabled } = useRoom();
   const userName = useSelector((state: RootState) => state.auth?.name);
   const userId = useSelector((state: RootState) => state.auth?.id);
 
   useEffect(() => {
     if (!(myPeer.id && myStream && roomId && userName)) return;
-    socket.emit("join-room", { roomId, peerId: myPeer.id, userName, userId });
+    socket.emit("join-room", { roomId, peerId: myPeer.id, userName, userId, isCameraEnabled, isMicEnabled });
 
     return () => {
       socket.off("join-room");
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [myPeer, myStream, roomId, userName, userId]);
 
   return (
