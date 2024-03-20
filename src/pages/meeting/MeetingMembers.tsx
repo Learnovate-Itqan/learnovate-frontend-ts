feb6d42ad8fc549a91025b6f844b01b07431b538
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-20 18:06:12 +0200

    Refactor MeetingMembers component to use CSS grid for layout

--- src/pages/meeting/MeetingMembers.tsx
+++ src/pages/meeting/MeetingMembers.tsx
@@ -8,15 +8,16 @@ export function MeetingMembers() {
   const { peers, shareScreenPeers } = useRoom();
   const myId = useSelector((state: RootState) => state.auth?.id);
   return (
-    <div className=" flex flex-col gap-3 grow m-2 mb-0 max-h-[calc(100dvh-90px)] overflow-y-auto overflow-x-hidden scrollbar">
-      <MeetingMember memberId={"you"} />
+    <div className=" grid gap-3 grow m-2 mb-0 max-h-[calc(100dvh-90px)] overflow-y-auto overflow-x-hidden scrollbar">
+      <MeetingMember memberId={"you"} className=" min-h-48" />
       {Object.keys(peers).map((userId) => {
         if (myId === userId) return null;
-        return <MeetingMember key={userId} memberId={peers[userId].userId} />;
+        return <MeetingMember key={userId} className=" min-h-48" memberId={peers[userId].userId} />;
       })}
       {Object.keys(shareScreenPeers).map((userId) => {
         return (
           <MeetingMember
+            className=" min-h-48"
             key={userId}
             isSharingScreen={true}
             memberId={shareScreenPeers[userId].userId === myId ? "you" : shareScreenPeers[userId].userId}
