f3b02381c6accf752191e750bf819c0385f419e6
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-22 00:21:15 +0200

    Refactor CalendarCarousel component to use selectedTime instead of selectedDate

--- src/pages/mentor/viewer/components/calendarCarousel.tsx
+++ src/pages/mentor/viewer/components/calendarCarousel.tsx
@@ -4,28 +4,32 @@ import { Button } from "@/components/ui/button";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 
 type CalendarCarouselProps = {
-  selectedDate: number[];
-  setSelectedDate: React.Dispatch<React.SetStateAction<number[]>>;
-  availability: number[];
+  selectedTime: number | undefined;
+  setSelectedTime: React.Dispatch<React.SetStateAction<number | undefined>>;
+  availability: { startTime: number; isBooked: boolean }[];
 };
-export const CalendarCarousel = ({ selectedDate, setSelectedDate, availability }: CalendarCarouselProps) => {
-  const handleSelection = (idx: number) =>
-    setSelectedDate((prev) => (prev.includes(idx) ? prev.filter((i) => i !== idx) : [...prev, idx]));
+export const CalendarCarousel = ({ selectedTime, setSelectedTime, availability }: CalendarCarouselProps) => {
+  const handleSelection = (idx: number) => setSelectedTime((prev) => (prev === idx ? undefined : idx));
   return (
     <Carousel className="flex flex-col gap-2">
-      <CarouselContent>
+      <CarouselContent className=" min-w-56">
         {availability?.map((time, index) => (
           <CarouselItem key={index} className="basis-1/2 ">
             <Button
-              variant={selectedDate.includes(time) ? "default" : "outline"}
-              className="w-full border"
-              onClick={() => handleSelection(time)}
+              variant={selectedTime === time.startTime ? "default" : "outline"}
+              className="border "
+              disabled={time.isBooked}
+              onClick={() => handleSelection(time.startTime)}
             >
-              {time > 12 ? `${time - 12}:00 PM` : `${time}:00 AM`}
+              {time.startTime > 12
+                ? `${time.startTime - 12}:00 PM`
+                : `${time.startTime === 0 ? "12" : time.startTime}:00 AM`}
             </Button>
           </CarouselItem>
         ))}
-        {availability.length < 1 && <CarouselItem>No available dates</CarouselItem>}
+        {availability.length < 1 && (
+          <CarouselItem className="text-center min-w-56 min-h-10">No available times at this date</CarouselItem>
+        )}
       </CarouselContent>
       <div className="w-full flex justify-between *:translate-y-0">
         <CarouselPrevious className="static" />
