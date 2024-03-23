cb021f002963e6995802c5b5298ec47fd1d08a77
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-23 22:16:55 +0200

    Add sessions data and update BookedSessions component in the mentor profile

--- src/pages/mentor/me/index.tsx
+++ src/pages/mentor/me/index.tsx
@@ -7,6 +7,80 @@ import { BookedSessions } from "./components/BookedSessions";
 import { MeStats } from "./components/meStats";
 import { MeHeader } from "./components/mentorMeHeader";
 
+const SESSIONS = [
+  {
+    id: 1,
+    date: new Date("2024-03-22"),
+    startTime: 10,
+    endTime: 11,
+    student: {
+      name: "Matthew Lane",
+    },
+    mentor: {
+      name: "Matthew Lane",
+    },
+  },
+  {
+    id: 2,
+    date: new Date("2024-03-23"),
+    startTime: 12,
+    endTime: 13,
+    student: {
+      name: "Mildred Waters",
+    },
+    mentor: {
+      name: "Mildred Waters",
+    },
+  },
+  {
+    id: 3,
+    date: new Date("2024-03-22"),
+    startTime: 14,
+    endTime: 15,
+    student: {
+      name: "David Ingram",
+    },
+    mentor: {
+      name: "David Ingram",
+    },
+  },
+  {
+    id: 4,
+    date: new Date("2024-03-22"),
+    startTime: 16,
+    endTime: 17,
+    student: {
+      name: "Duane Cruz",
+    },
+    mentor: {
+      name: "Duane Cruz",
+    },
+  },
+  {
+    id: 5,
+    date: new Date("2024-03-23"),
+    startTime: 18,
+    endTime: 19,
+    student: {
+      name: "Gene Cummings",
+    },
+    mentor: {
+      name: "Gene Cummings",
+    },
+  },
+  {
+    id: 6,
+    date: new Date("2024-03-24"),
+    startTime: 20,
+    endTime: 21,
+    student: {
+      name: "Edgar Rogers",
+    },
+    mentor: {
+      name: "Edgar Rogers",
+    },
+  },
+];
 export const MentorMePage = () => {
   const skills = mentor.skills.map((skill) => skill.name);
 
@@ -31,7 +105,7 @@ export const MentorMePage = () => {
           </div>
           <div className="my-8 flex flex-col md:flex-row gap-y-8 gap-x-4 justify-between">
             <AvailabilityEditor />
-            <BookedSessions />
+            <BookedSessions sessions={SESSIONS} userRole="mentor" />
           </div>
         </div>
       </main>
