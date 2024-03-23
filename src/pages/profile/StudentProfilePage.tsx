ffe25658f7723e19c09474eb78416f019b3f5718
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-23 21:08:03 +0200

    Add TracksProgress component and update layout

--- src/pages/profile/StudentProfilePage.tsx
+++ src/pages/profile/StudentProfilePage.tsx
@@ -3,25 +3,51 @@ import { SocialCard } from "@/components/ui/SocialCard";
 
 import { StudentHeader } from "./components/StudentHeader";
 import { StudentInfo } from "./components/StudentInfo";
+import { TracksProgress } from "./components/TracksProgress";
+
+const tracks = [
+  {
+    id: "1",
+    title: "Frontend",
+    progress: 50,
+  },
+  {
+    id: "2",
+    title: "Backend",
+    progress: 30,
+  },
+  {
+    id: "3",
+    title: "DevOps",
+    progress: 70,
+  },
+  {
+    id: "4",
+    title: "UI/UX",
+    progress: 90,
+  },
+];
 
 export function StudentProfilePage() {
   return (
     <main>
       <StudentHeader name="mohamed Ebrahim" studentImage={studentImageDefault} id="2135464646645" />
       <main className="container pb-20">
-        <section className="flex flex-col gap-5">
+        <section className="flex grow flex-col gap-5 lg:flex-row">
           <StudentInfo
             education="Bachelor's degree in Computer Science from XYZ University."
             dateOfBirth={new Date("2001-02-27")}
             location="Egypt"
           />
-          
-          <SocialCard
-            mail="Learnovate@gmail.com"
-            github="Github.com/learnovate"
-            linkedin="Linkedin.com/learnovate"
-            facebook="Facebook.com/learnovate"
-          />
+          <div className="grid gap-5 sm:grid-cols-2 lg:max-w-xl">
+            <TracksProgress tracks={tracks} />
+            <SocialCard
+              mail="Learnovate@gmail.com"
+              github="Github.com/learnovate"
+              linkedin="Linkedin.com/learnovate"
+              facebook="Facebook.com/learnovate"
+            />
+          </div>
         </section>
       </main>
     </main>
