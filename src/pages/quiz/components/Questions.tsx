a376fcb7a4972db2e4aee8189ba7473e23eb068b
kareem172 <kareemkhalaf1722@gmail.com>
2024-03-24 17:40:03 +0200

    Add quiz components

---
+++ src/pages/quiz/components/Questions.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
+
+import Question from "./Question";
+
+type QuestionsProps = {
+  questions: { question: string; options: string[]; points: number }[];
+};
+type Answers = {
+  [question: string]: { answer: string | null; points: number };
+};
+
+export default function Questions({ questions }: QuestionsProps) {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState<Answers>(() => {
+    return questions.reduce((acc, question) => {
+      acc[question.question] = { answer: null, points: 0 };
+      return acc;
+    }, {} as Answers);
+  });
+
+  const answeredQuestions = Object.values(answers).filter((answer) => answer.answer !== null).length;
+  function handleNextQuestion() {
+    if (currentQuestion === questions.length - 1) return;
+    setCurrentQuestion((prev) => prev + 1);
+  }
+  function handlePreviousQuestion() {
+    if (currentQuestion === 0) return;
+    setCurrentQuestion((prev) => prev - 1);
+  }
+  function handleAnswerChange(question: string, value: string) {
+    setAnswers((prev) => {
+      return {
+        ...prev,
+        [question]: { answer: value, points: questions[currentQuestion].points },
+      };
+    });
+  }
+  return (
+    <>
+      <Progress
+        className=" h-3 bg-zinc-200 text-white shadow-2xl"
+        value={(answeredQuestions / questions.length) * 100}
+      />
+
+      <section className="grid gap-5 py-10">
+        <Question
+          question={questions[currentQuestion].question}
+          options={questions[currentQuestion].options}
+          currentQuestionNumber={currentQuestion + 1}
+          questionsCount={questions.length}
+          pointsPerQuestion={questions[currentQuestion].points}
+          onAnswerChange={handleAnswerChange}
+          currentAnswer={answers[questions[currentQuestion].question].answer}
+        />
+      </section>
+      <div className=" flex justify-end gap-2">
+        <Button
+          size="lg"
+          variant="ghost"
+          className=" text-royal-blue hover:text-royal-blue"
+          onClick={handlePreviousQuestion}
+        >
+          Previous
+        </Button>
+        <Button size="lg" onClick={handleNextQuestion}>
+          {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+        </Button>
+      </div>
+    </>
+  );
+}
