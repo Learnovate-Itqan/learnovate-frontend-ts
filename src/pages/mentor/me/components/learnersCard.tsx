4fb2f57a8b4c04bee7c3067e67883455f487c779
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-06 00:03:28 +0200

    Refactor learnersCard component styling

--- src/pages/mentor/me/components/learnersCard.tsx
+++ src/pages/mentor/me/components/learnersCard.tsx
@@ -2,8 +2,8 @@ import { Learners } from "@/components/icons/Learners";
 
 export const LearnersCard = ({ learnersNumber }: { learnersNumber: number }) => {
   return (
-    <div className="shadow-custom flex flex-col items-center gap-y-2 p-6 rounded-md">
-      <Learners className="w-32" fill="#3498DB" />
+    <div className="shadow-custom flex grow flex-col items-center gap-y-2 p-6 px-10 rounded-md">
+      <Learners className="w-24" fill="#3498DB" />
       <h5 className="text-royal-blue text-xl">Learners</h5>
       <p className="text-3xl">{learnersNumber}</p>
     </div>
