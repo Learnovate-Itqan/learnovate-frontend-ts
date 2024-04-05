229376057a3f659b7c98dedf554b94752d470977
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-05 23:16:40 +0200

    Update learners number dynamically

--- src/pages/mentor/me/components/learnersCard.tsx
+++ src/pages/mentor/me/components/learnersCard.tsx
@@ -1,11 +1,11 @@
 import { Learners } from "@/components/icons/Learners";
 
-export const LearnersCard = () => {
+export const LearnersCard = ({ learnersNumber }: { learnersNumber: number }) => {
   return (
     <div className="shadow-custom flex flex-col items-center gap-y-2 p-6 rounded-md">
       <Learners className="w-32" fill="#3498DB" />
       <h5 className="text-royal-blue text-xl">Learners</h5>
-      <p className="text-3xl">203</p>
+      <p className="text-3xl">{learnersNumber}</p>
     </div>
   );
 };
