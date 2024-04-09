53bcd8801545c21a7b52f83ca40f19d5509945e8
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-09 13:29:19 +0200

    Add imageSchema and pdfSchema for file validation

---
+++ src/schemas/imageSchema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const MAX_FILE_SIZE = 1024 * 1024 * 5;
+const ACCEPTED_IMAGE_MIME_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+
+export const imageSchema = z
+  .any()
+  .refine((file: File) => file?.size > 0, "Image is required")
+  .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+  .refine(
+    (file) => ACCEPTED_IMAGE_MIME_TYPES.includes(file?.type),
+    "Only .jpg, .jpeg, .png and .webp formats are supported."
+  );
