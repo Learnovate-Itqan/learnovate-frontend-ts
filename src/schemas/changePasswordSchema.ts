d60fbaf4cd849f1d37de93f3f1335830777a2f66
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-14 15:12:27 +0200

    Update changePasswordSchema to validate password only if the user tried to type a password

--- src/schemas/changePasswordSchema.ts
+++ src/schemas/changePasswordSchema.ts
@@ -2,17 +2,21 @@ import { z } from "zod";
 
 export const changePasswordSchema = z
   .object({
-    oldPassword: z
-      .string()
-      .min(8, "password must be at least 8 characters")
-      .max(64, "name can not exceed 64 characters"),
-    newPassword: z
-      .string()
-      .min(8, "password must be at least 8 characters")
-      .max(64, "name can not exceed 64 characters"),
-    confirmPassword: z.string().min(1, "confirm password can not be empty"),
+    oldPassword: z.string().max(64, "password can not exceed 64 characters"),
+    newPassword: z.string().max(64, "password can not exceed 64 characters"),
+    confirmPassword: z.string(),
   })
-  .refine((data) => data.newPassword === data.confirmPassword, {
-    message: "passwords do not match",
-    path: ["confirmPassword"],
-  });
+  .refine((data) => data.newPassword.length === 0 || data.newPassword.length > 8, {
+    message: "password must be at least 8 characters",
+    path: ["newPassword"],
+  })
+  .refine(
+    (data) => {
+      if (data.newPassword.length === 0) return true;
+      return data.newPassword === data.confirmPassword;
+    },
+    {
+      message: "passwords do not match",
+      path: ["confirmPassword"],
+    }
+  );
