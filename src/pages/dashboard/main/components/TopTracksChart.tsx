d24e04867f2131338456f65f7d09a7ab115b7eb2
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-18 18:09:52 +0200

    Add TopTracksChart component for displaying top tracks in PieChart in the dashboard

---
+++ src/pages/dashboard/main/components/TopTracksChart.tsx
@@ -0,0 +1,46 @@
+import { Cell, Label, Legend, Pie, PieChart, ResponsiveContainer } from "recharts";
+
+const COLORS = ["#181D3C", "#3498DB", "#DEE12D", "#3734DB", "#2ECC71", "#F93C65"];
+type Data = {
+  name: string;
+  value: number;
+};
+
+export function DashboardPieChart({ data }: { data: Data[] }) {
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <PieChart width={400} height={400}>
+        <Pie
+          data={data}
+          cx={180}
+          cy={200}
+          innerRadius={110}
+          outerRadius={150}
+          fill="#8884d8"
+          paddingAngle={2}
+          dataKey="value"
+        >
+          {data.map((_entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+          <Label position="center" value={"Top Tracks"} fill="#000" className="text-center text-3xl font-semibold" />
+        </Pie>
+        <Legend
+          verticalAlign="middle"
+          wrapperStyle={{ lineHeight: 2.5 }}
+          iconType="circle"
+          layout="vertical"
+          align="right"
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+}
+
+export function TopTracksChart({ data }: { data: Data[] }) {
+  return (
+    <div className="rounded-xl shadow-custom">
+      <DashboardPieChart data={data} />
+    </div>
+  );
+}
