53ce6b44112138d630689e98ba2e562089602a99
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-21 03:53:57 +0200

    Refactor DashboardMentors component to include search functionality and MentorsTable in the dashboard and pagination

--- src/pages/dashboard/mentors/index.tsx
+++ src/pages/dashboard/mentors/index.tsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useDebouncedCallback } from "use-debounce";
+
+import { Paginate } from "@/components/ui/Paginate";
+import { SearchBar } from "@/components/ui/SearchBar";
+import { useGetData } from "@/hooks/useApi";
+
+import { MentorsTable } from "../components/MentorTable";
+
 export function DashboardMentors() {
-  return <div>DashboardMentors</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [search, setSearch] = useState(searchParams.get("search") || "");
+  const debounceSearch = useDebouncedCallback((value) => {
+    searchParams.set("search", value);
+    setSearchParams(searchParams);
+  }, 500);
+
+  // fetch Courses
+  const { data: response } = useGetData(`dashboard/courses?${searchParams.toString()}`);
+  const { data } = response || {};
+  const { courses } = data || {};
+  return (
+    <main>
+      <section className=" shadow-custom rounded-xl py-6 mb-10">
+        <header className="flex justify-between items-center px-6 mb-5">
+          <h1 className=" text-2xl font-semibold ">Mentors</h1>
+          <div className="flex gap-2 justify-end items-center">
+            <SearchBar
+              value={search}
+              onChange={(value) => {
+                setSearch(value);
+                debounceSearch(value);
+              }}
+              className="min-w-48 bg-gray-100"
+            />
+           
+          </div>
+        </header>
+        <MentorsTable mentors={courses} />
+      </section>
+      <Paginate pageCount={3} />
+    </main>
+  );
 }
