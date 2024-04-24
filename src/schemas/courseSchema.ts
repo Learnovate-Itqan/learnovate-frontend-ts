462f834f85176cc384ca39cc08fbe79684145dcc
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-24 17:43:37 +0200

    Refactor courseSchema to include basicCourseInfoSchema and courseChaptersSchema in courseSchema.ts

--- src/schemas/courseSchema.ts
+++ src/schemas/courseSchema.ts
@@ -1,25 +1,41 @@
 import { z } from "zod";
 
-export const courseSchema = z.object({
-  id: z.string().uuid(),
-  title: z.string(),
-  publishTime: z.string(),
-  description: z.string(),
-  keywords: z.array(z.string()),
-  image: z.string().url(),
-  progress: z.number(),
+import { imageSchema } from "./imageSchema";
+
+export const courseChaptersSchema = z.object({
+  chapters: z.array(
+    z.object({
+      title: z.string().min(1, { message: "Title is required" }),
+      link: z.string().url({ message: "Invalid URL" }),
+    })
+  ),
+});
+export const basicCourseInfoSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+  description: z.string().min(1, { message: "Description is required" }),
+  keywords: z.array(z.string()).min(2, "At least 2 keywords are required").max(8, "Maximum 8 keywords are allowed"),
+  image: imageSchema,
+  trackName: z.string(),
+  cLevel: z.literal("beginner").or(z.literal("intermediate")).or(z.literal("hard")),
   estimatedTime: z.number(),
   noChapters: z.number(),
-  noStudentsEnrolled: z.number(),
-  rating: z.number(),
-  cLevel: z.literal("beginner").or(z.literal("intermediate")).or(z.literal("hard")),
-  cLink: z.string(),
-  trackID: z.string().uuid(),
-  trackName: z.string(),
   price: z.number(),
-  publisher: z.object({
-    id: z.string().uuid(),
-    name: z.string(),
-    image: z.string().url(),
-  }),
 });
+
+export const courseSchema = z
+  .object({
+    id: z.string().uuid(),
+    publishTime: z.string(),
+    progress: z.number(),
+    noStudentsEnrolled: z.number(),
+    rating: z.number(),
+    cLink: z.string(),
+    trackID: z.string().uuid(),
+    publisher: z.object({
+      id: z.string().uuid(),
+      name: z.string(),
+      image: z.string().url(),
+    }),
+  })
+  .merge(basicCourseInfoSchema)
+  .merge(courseChaptersSchema);
