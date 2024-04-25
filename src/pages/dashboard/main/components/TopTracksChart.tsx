6a6885c391eb737bb66a1ed7de632b9b482ba075
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-25 02:58:40 +0200

    Refactor TopTracksChart component to improve responsiveness and styling

--- src/pages/dashboard/main/components/TopTracksChart.tsx
+++ src/pages/dashboard/main/components/TopTracksChart.tsx
@@ -9,25 +9,21 @@ type Data = {
 export function DashboardPieChart({ data }: { data: Data[] }) {
   return (
     <ResponsiveContainer width="100%" height={400}>
-      <PieChart width={400} height={400}>
-        <Pie
-          data={data}
-          cx={180}
-          cy={200}
-          innerRadius={110}
-          outerRadius={150}
-          fill="#8884d8"
-          paddingAngle={2}
-          dataKey="value"
-        >
+      <PieChart>
+        <Pie data={data} cx={"50%"} cy={"50%"} innerRadius={"50%"} fill="#8884d8" paddingAngle={2} dataKey="value">
           {data.map((_entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
-          <Label position="center" value={"Top Tracks"} fill="#000" className="text-center text-3xl font-semibold" />
+          <Label
+            position="center"
+            value={"Top Tracks"}
+            fill="#000"
+            className="text-center sm:text-xl md:text-2xl  font-semibold"
+          />
         </Pie>
         <Legend
           verticalAlign="middle"
-          wrapperStyle={{ lineHeight: 2.5 }}
+          wrapperStyle={{ lineHeight: 2.5, textAlign: "center", fontSize: ".8rem"}}
           iconType="circle"
           layout="vertical"
           align="right"
@@ -39,7 +35,7 @@ export function DashboardPieChart({ data }: { data: Data[] }) {
 
 export function TopTracksChart({ data }: { data: Data[] }) {
   return (
-    <div className="rounded-xl shadow-custom">
+    <div className="rounded-xl shadow-custom ">
       <DashboardPieChart data={data} />
     </div>
   );
