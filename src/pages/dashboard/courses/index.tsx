0bfe68bfc9566f969a8ed6dcc29e7c99e0efff1a
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-25 03:49:44 +0200

    Update DashboardCourses component to include responsive styling for large screens

--- src/pages/dashboard/courses/index.tsx
+++ src/pages/dashboard/courses/index.tsx
@@ -27,9 +27,9 @@ export function DashboardCourses() {
   return (
     <main>
       <section className=" shadow-custom rounded-xl py-6 mb-10">
-        <header className="flex flex-col gap-2 md:flex-row md:justify-between md:items-center px-6 mb-5">
+        <header className="flex flex-col gap-2 lg:flex-row lg:justify-between lg:items-center px-6 mb-5">
           <h1 className=" text-2xl font-semibold ">Courses</h1>
-          <div className="flex gap-2 justify-between md:justify-end items-center">
+          <div className="flex gap-2 justify-between lg:justify-end items-center">
             <SearchBar
               value={search}
               onChange={(value) => {
