bd8dc3f14e7ed7e300a4724df8d0a607d3685e2c
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-28 07:05:06 +0300

    Add joinMeeting function to BookedSession component for meeting navigation

--- src/components/ui/bookedSession.tsx
+++ src/components/ui/bookedSession.tsx
@@ -1,5 +1,9 @@
 import { format } from "date-fns";
 import { IoIosArrowForward } from "react-icons/io";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { RootState } from "@/redux/store";
 
 import { Button } from "./button";
 
@@ -7,9 +11,16 @@ type BookedSessionProps = {
   startTime: number;
   endTime: number;
   meetingWith: string;
+  roomId: string;
 };
 
-export default function BookedSession({ startTime, endTime, meetingWith }: BookedSessionProps) {
+export default function BookedSession({ startTime, endTime, meetingWith, roomId }: BookedSessionProps) {
+  const navigate = useNavigate();
+  const user = useSelector((state: RootState) => state.auth);
+  function joinMeeting() {
+    if (!user.authStatus) return;
+    navigate(`/meeting/${roomId}`);
+  }
   return (
     <div className="flex justify-between bg-zinc-100 px-3 py-3 rounded-lg items-baseline gap-4">
       <div className="flex gap-1 flex-col">
@@ -18,7 +29,7 @@ export default function BookedSession({ startTime, endTime, meetingWith }: Booke
           {format(new Date().setHours(startTime), "hh:00 a")} - {format(new Date().setHours(endTime), "hh:00 a")}
         </span>
       </div>
-      <Button variant="link" className="text-wrap flex items-center  text-royal-blue">
+      <Button variant="link" className="text-wrap flex items-center  text-royal-blue" onClick={joinMeeting}>
         Go to meeting
         <IoIosArrowForward className=" hidden md:block" />
       </Button>
