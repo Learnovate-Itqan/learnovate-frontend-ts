a579c7d8e0b6907610b4396647b9de91ecf94bfc
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-28 15:00:09 +0300

    Refactor layout components to improve code organization and reusability

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,14 +1,9 @@
 import { Outlet } from "react-router-dom";
 
-import { Footer } from "./Footer";
-import { Navbar } from "./Navbar";
-
 export function AppLayout() {
   return (
     <main className="grid relative min-h-dvh">
-      <Navbar />
       <Outlet />
-      <Footer />
     </main>
   );
 }
