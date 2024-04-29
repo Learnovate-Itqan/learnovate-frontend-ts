0abf882af7d854a5803490174fb667f2a3e6d8a5
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-29 16:58:35 +0300

    Refactor AppLayout component to handle user authentication and dispatch setUser action

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,19 +1,15 @@
 import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
-import { z } from "zod";
 
 import { useGetData } from "@/hooks/useApi";
 import { setUser } from "@/redux/slices/authSlice";
-import { userSchema } from "@/schemas/userSchema";
 
 export function AppLayout() {
   const dispatch = useDispatch();
-  const { data } = useGetData("/me");
-  const { user, authStatus }: { user: z.infer<typeof userSchema> | undefined; authStatus: boolean } = data?.data || {
-    user: undefined,
-    authStatus: false,
-  };
-  if (!authStatus) {
+  const { data: response } = useGetData("/nav");
+  const { user: data } = response?.data || {};
+  const { loggedIn: authStatus, data: user } = data || {};
+  if (authStatus === false) {
     localStorage.removeItem("token");
   }
   if (authStatus && user) {
