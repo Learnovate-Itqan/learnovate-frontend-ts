495835bf365bde7a9f6ce41489d02edd1406a5fc
kareem172 <kareemkhalaf1722@gmail.com>
2024-04-29 16:58:44 +0300

    Refactor useTracksName hook to use useGetData hook for fetching tracks data

--- src/hooks/useTracksName.tsx
+++ src/hooks/useTracksName.tsx
@@ -1,17 +1,12 @@
-import { useQueryClient } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
 import { z } from "zod";
 
 import { trackSchema } from "@/schemas/trackSchema";
 
-export function useTracksName() {
-  const queryClient = useQueryClient();
-  const tracksQuery = queryClient.getQueryData(["tracks"]) as z.infer<typeof trackSchema>[] | null;
-  const [tracks, setTracks] = useState<string[]>([]);
-  useEffect(() => {
-    if (tracksQuery) {
-      setTracks(tracksQuery.map((track) => track.name));
-    }
-  }, [tracksQuery]);
+import { useGetData } from "./useApi";
+
+export function useTracks() {
+  const { data: response } = useGetData("/nav");
+  const { tracks }: { tracks: z.infer<typeof trackSchema>[] } = response?.data || {};
+
   return tracks;
 }
