365bced7b847418b19e58eb68a61445c6ca02983
kareem172 <kareemkhalaf1722@gmail.com>
2024-05-02 02:33:04 +0300

    Update studentSchema.ts to make mobileNumber, dateOfBirth, and gradYear fields optional

--- src/schemas/studentSchema.ts
+++ src/schemas/studentSchema.ts
@@ -28,10 +28,10 @@ export const studentBasicInfoFormSchema = z.object({
     .email({ message: "Invalid email" })
     .min(1, { message: "Email is required" })
     .max(100, { message: "Email is too long" }),
-  mobileNumber: z.string().max(100, { message: "Mobile number is too long" }),
-  dateOfBirth: z.date(),
+  mobileNumber: z.string().max(100, { message: "Mobile number is too long" }).optional(),
+  dateOfBirth: z.date().optional(),
   country: z.string().max(100, { message: "Country is too long" }),
   city: z.string().max(100, { message: "City is too long" }),
   education: z.string().max(100, { message: "Education is too long" }),
-  gradYear: z.number(),
+  gradYear: z.number().optional(),
 });
