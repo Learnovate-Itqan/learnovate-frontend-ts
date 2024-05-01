ebaaf195fb5228b42da14bdcb5011ad65dd05d33
kareem172 <kareemkhalaf1722@gmail.com>
2024-05-01 11:31:13 +0300

    Add authentication check and redirect to login page in WishListButton component

--- src/components/ui/WishListButton.tsx
+++ src/components/ui/WishListButton.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import { usePatchData } from "@/hooks/useApi";
 import { cn } from "@/lib/utils";
+import { RootState } from "@/redux/store";
 
 type WishListButtonProps = {
   className?: string;
@@ -10,9 +14,16 @@ type WishListButtonProps = {
   isWishListed: boolean;
 };
 export function WishListButton({ className = "", courseId, isWishListed }: WishListButtonProps) {
+  const navigate = useNavigate();
+  const authStatus = useSelector((state: RootState) => state.auth.authStatus);
   const [optimisticWish, setOptimisticWish] = useState(isWishListed);
   const addToWishList = usePatchData(`/courses/${courseId}/wishlist`);
   async function handleWishList() {
+    if (!authStatus) {
+      toast.error("Please login to add to wishlist");
+      navigate("/auth/login");
+      return;
+    }
     setOptimisticWish((prev) => !prev);
     const { status } = await addToWishList.mutateAsync({ wishList: !optimisticWish });
     if (status === "failed") {
