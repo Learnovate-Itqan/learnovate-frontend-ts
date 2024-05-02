9474422cc8ebf7e9c4204d5e11ac969f886bd9f4
kareem172 <kareemkhalaf1722@gmail.com>
2024-05-02 16:54:05 +0300

    Add optional "phoneNumber" field to userSchema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -10,6 +10,7 @@ export const userSchema = z.object({
   age: z.union([z.number().int().positive(), z.null()]).optional(),
   dob: z.union([z.date(), z.null()]).optional(),
   gender: z.union([z.literal("male"), z.literal("female"), z.null()]).optional(),
+  phoneNumber: z.string().max(100, { message: "Mobile number is too long" }).optional(),
   country: z.union([z.string(), z.null()]).optional(),
   city: z.union([z.string(), z.null()]).optional(),
   bio: z.union([z.string(), z.null()]).optional(),
