79f181c286f677ca8bf28cb3486d4bdc1db5004e
kareem172 <kareemkhalaf1722@gmail.com>
2024-05-02 16:58:19 +0300

    add update functionality and fetching the default values

--- src/pages/editProfile/StudentEditProfile.tsx
+++ src/pages/editProfile/StudentEditProfile.tsx
@@ -1,13 +1,16 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useMemo } from "react";
+import { AxiosResponse } from "axios";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { z } from "zod";
 
 import { ImageUploader } from "@/components/ui/ImageUploader";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
-import { useGetData } from "@/hooks/useApi";
+import { getRequest, usePatchData } from "@/hooks/useApi";
+import { LoadingPage } from "@/layouts/LoadingPage";
 import { changePasswordSchema } from "@/schemas/changePasswordSchema";
 import { SocialMediaSchema } from "@/schemas/mentorSchema";
 import { studentBasicInfoFormSchema, studentSchema } from "@/schemas/studentSchema";
@@ -21,43 +24,96 @@ export type TStudentEditProfileForm = z.infer<typeof studentBasicInfoFormSchema>
   z.infer<typeof SocialMediaSchema>;
 
 export function StudentEditProfile() {
-  const { data: response } = useGetData("/students/profile");
-  const { data } = response?.data || {};
-  const { student }: { student: z.infer<typeof studentSchema> } = data || {};
-  const defaultValues = useMemo(
-    () => ({
-      name: student?.user?.name,
-      email: student?.user?.email,
-      mobileNumber: student?.mobileNumber || "",
-      country: student?.user?.country || "",
-      city: student?.user?.city || "",
-      education: student?.education || "",
-      gradYear: student?.gradYear || undefined,
-      facebook: student?.facebook || "",
-      linkedIn: student?.linkedIn || "",
-      github: student?.gitHub || "",
-      oldPassword: "",
-      newPassword: "",
-      confirmPassword: "",
-      image: student?.user?.image || "",
-      dateOfBirth: student?.user?.dateOfBirth || undefined,
-    }),
-    [student]
-  );
+  const [studentId, setStudentId] = useState<string | null>(null);
   const navigate = useNavigate();
+
   const editForm = useForm<TStudentEditProfileForm>({
     resolver: zodResolver(studentBasicInfoFormSchema.extend(SocialMediaSchema.shape).and(changePasswordSchema)),
-    defaultValues,
+    defaultValues: async () => {
+      const { data: response } = (await getRequest("/students/profile")) as AxiosResponse;
+      const { student }: { student: z.infer<typeof studentSchema> } = response?.data || {};
+      console.log(student);
+      if (student) {
+        setStudentId(student?.id);
+      }
+      return {
+        name: student?.user?.name,
+        email: student?.user?.email,
+        phoneNumber: student?.user.phoneNumber || "",
+        country: student?.user?.country || "",
+        city: student?.user?.city || "",
+        education: student?.education || "",
+        graduationYear: student?.graduationYear || undefined,
+        facebook: student?.facebook || "",
+        linkedIn: student?.linkedIn || "",
+        github: student?.gitHub || "",
+        oldPassword: "",
+        newPassword: "",
+        confirmPassword: "",
+        image: student?.user?.image || "",
+        dob: student?.user?.dob || undefined,
+        bio: student?.user?.bio || "",
+      };
+    },
   });
 
-  async function handleBasicInfoSubmit(data: TStudentEditProfileForm) {
-    console.log("submitted", data);
-  }
-  useEffect(() => {
-    editForm.reset(defaultValues);
-  }, [defaultValues, editForm]);
+  const updateStudent = usePatchData(`students/${studentId}`);
+  const updatePassword = usePatchData(`users/updatepassword`);
+  const updateImage = usePatchData(`users/upload/image`, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
-  if (!student) return null;
+  async function handleBasicInfoSubmit(formData: TStudentEditProfileForm) {
+    if (!editForm.formState.isDirty) {
+      toast.error("No changes made to be updated");
+      return;
+    }
+    const toastId = toast.loading("Updating profile...");
+    const { oldPassword, newPassword, confirmPassword, image, ...rest } = formData;
+    const updateData = { ...rest };
+    if (oldPassword && newPassword && confirmPassword) {
+      const passwordData = {
+        oldPassword,
+        newPassword,
+        confirmPassword,
+      };
+      const passwordResponse = await updatePassword.mutateAsync(passwordData);
+      if (passwordResponse?.status === "failed") {
+        toast.error(passwordResponse?.data?.errors[0].msg, { id: toastId });
+        return;
+      }
+    }
+    if (editForm.formState.dirtyFields.image) {
+      const file = new FormData();
+      file.append("image", image);
+      const imageResponse = await updateImage.mutateAsync(file);
+      if (imageResponse?.status === "failed") {
+        toast.error(imageResponse?.data?.errors[0].msg, { id: toastId });
+        return;
+      }
+    }
+    if (editForm.formState.dirtyFields) {
+      const response = await updateStudent.mutateAsync(updateData);
+      if (response?.status === "failed") {
+        toast.error(response?.data?.errors[0].msg, { id: toastId });
+        return;
+      } else {
+        toast.success("Profile updated successfully", { id: toastId });
+      }
+    }
+
+    const response = await updateStudent.mutateAsync(updateData);
+    if (response?.status === "failed") {
+      toast.error(response?.data?.errors[0].msg, { id: toastId });
+      return;
+    } else {
+      toast.success("Profile updated successfully", { id: toastId });
+    }
+  }
+
+  if (editForm.formState.isLoading) return <LoadingPage />;
   return (
     <main className=" container py-10">
       <header className="mb-5">
@@ -67,7 +123,7 @@ export function StudentEditProfile() {
         <Form {...editForm}>
           <form
             className="grid lg:grid-cols-[250px_1fr] gap-10"
-            onSubmit={editForm.handleSubmit(handleBasicInfoSubmit)}
+            onSubmit={editForm.handleSubmit(handleBasicInfoSubmit, (errors) => console.log(errors))}
           >
             <aside className="flex flex-col items-center gap-2 mb-10 ">
               <FormField
@@ -83,8 +139,8 @@ export function StudentEditProfile() {
                 )}
               />
 
-              <h1 className="font-semibold text-2xl">{student.user.name}</h1>
-              <p className="text-zinc-400">{student.user.email}</p>
+              <h1 className="font-semibold text-2xl">{editForm.formState.defaultValues?.name}</h1>
+              <p className="text-zinc-400">{editForm.formState.defaultValues?.email}</p>
             </aside>
             <section className=" space-y-10">
               <div>
