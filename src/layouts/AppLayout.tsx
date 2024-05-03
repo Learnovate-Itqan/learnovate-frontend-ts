11a88c13e75571b8629c620739a92925de6d442f
kareem172 <kareemkhalaf1722@gmail.com>
2024-05-03 17:05:44 +0300

    Refactor AppLayout component to handle loading state and dispatch setUser action in useEffect

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
 
@@ -8,14 +9,19 @@ import { setUser } from "@/redux/slices/authSlice";
 import { LoadingPage } from "./LoadingPage";
 import { Navbar } from "./Navbar";
 
+let renderCount = 0;
 export function AppLayout() {
   const dispatch = useDispatch();
   const { data: response } = useGetData("/nav");
   const { user: data } = response?.data || {};
   const { loggedIn: authStatus, data: user } = data || {};
-  if (authStatus && user) {
-    dispatch(setUser({ ...user, authStatus }));
-  }
+
+  useEffect(() => {
+    if (authStatus && user) {
+      dispatch(setUser({ ...user, authStatus }));
+    }
+  }, [authStatus, user, dispatch]);
+
   if (!response) {
     return (
       <>
@@ -24,6 +30,7 @@ export function AppLayout() {
       </>
     );
   }
+  console.log("AppLayout rendered: ", ++renderCount);
   return (
     <main className="grid relative min-h-dvh">
       <ScrollToTop />
