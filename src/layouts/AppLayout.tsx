0cdafba61830b3adca8612d930e6beffb54801e5
kareem172 <kareemkhalaf1722@gmail.com>
2024-05-03 16:43:55 +0300

    Refactor AppLayout component to handle loading state and display Navbar and LoadingPage components

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -5,18 +5,25 @@ import ScrollToTop from "@/components/ui/ScrollToTop";
 import { useGetData } from "@/hooks/useApi";
 import { setUser } from "@/redux/slices/authSlice";
 
+import { LoadingPage } from "./LoadingPage";
+import { Navbar } from "./Navbar";
+
 export function AppLayout() {
   const dispatch = useDispatch();
   const { data: response } = useGetData("/nav");
   const { user: data } = response?.data || {};
   const { loggedIn: authStatus, data: user } = data || {};
-  if (authStatus === false) {
-    localStorage.removeItem("token");
-  }
   if (authStatus && user) {
     dispatch(setUser({ ...user, authStatus }));
   }
-
+  if (!response) {
+    return (
+      <>
+        <Navbar />
+        <LoadingPage />
+      </>
+    );
+  }
   return (
     <main className="grid relative min-h-dvh">
       <ScrollToTop />
