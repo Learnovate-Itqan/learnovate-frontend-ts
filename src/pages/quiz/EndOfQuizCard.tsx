b2ba3d01330587ff95b242ae14425875df6c4302
kareem172 <kareemkhalaf1722@gmail.com>
2024-05-05 13:17:41 +0300

    add EndOfQuizCard component to display quiz results and provide navigation options

---
+++ src/pages/quiz/EndOfQuizCard.tsx
@@ -0,0 +1,60 @@
+import { CircularProgressbarWithChildren } from "react-circular-progressbar";
+import { useNavigate } from "react-router-dom";
+
+import { Button } from "@/components/ui/button";
+
+function scoreMessage(score: number) {
+  if (score < 50) {
+    return "No worries! Keep studying and you'll get there!";
+  } else if (score < 70) {
+    return "You're halfway there! Keep up the good work";
+  } else if (score < 90) {
+    return "Keep studying to solidify your knowledge!";
+  } else {
+    return "Fantastic job! You're really mastering this material";
+  }
+}
+export function EndOfQuizCard() {
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col items-center gap-3 p-5">
+      <CircularProgressbarWithChildren
+        styles={{
+          path: {
+            stroke: "#3498DB",
+            strokeWidth: 5,
+          },
+          trail: {
+            stroke: "#D9D9D9",
+            strokeWidth: 5,
+          },
+        }}
+        className=" -rotate-[125deg] max-w-56"
+        value={80}
+        circleRatio={0.7}
+      >
+        <div className="flex flex-col items-center gap-1">
+          <div>
+            <span className="text-6xl font-bold">80</span>
+            <span className="text-4xl font-bold">%</span>
+          </div>
+          <span className="text-lg text-neutral-gray">Your score</span>
+        </div>
+      </CircularProgressbarWithChildren>
+      <p className="text-center text-2xl font-medium">{scoreMessage(80)}</p>
+      <div>
+        <h3 className=" text-neutral-gray">We recommend courses based on your level</h3>
+      </div>
+      <div className=" grid grid-cols-2 gap-2">
+        <Button
+          onClick={() => navigate("/")}
+          className=" text-royal-blue border-royal-blue hover:text-royal-blue"
+          variant={"outline"}
+        >
+          home
+        </Button>
+        <Button onClick={() => navigate("/roadmap/5")}>Generate RoodMap</Button>
+      </div>
+    </div>
+  );
+}
