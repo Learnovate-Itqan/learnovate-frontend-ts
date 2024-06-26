5803bf31142f5600cb8fcd353cd1048347ed8904
kareem172 <kareemkhalaf1722@gmail.com>
2024-06-26 03:46:48 +0300

    refactor: Improve AddCourseForm component and connect ot to the server

--- src/pages/dashboard/courses/components/AddCourseForm.tsx
+++ src/pages/dashboard/courses/components/AddCourseForm.tsx
@@ -1,7 +1,9 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
+import toast from "react-hot-toast";
 import { z } from "zod";
 
+import { usePostData } from "@/hooks/useApi";
 import { basicCourseInfoSchema, courseChaptersSchema } from "@/schemas/courseSchema";
 
 import { CourseChaptersForm } from "./CourseChaptersForm";
@@ -11,14 +13,34 @@ export function AddCourseForm({ onCloseModal }: { onCloseModal?: () => void }) {
   const [currentStep, setCurrentStep] = useState(0);
   const [prevStep, setPrevStep] = useState(0);
   const [courseInfo, setCourseInfo] = useState<z.infer<typeof basicCourseInfoSchema>>();
+  const createCourse = usePostData("admin/courses");
+
   function handleCourseInfo(data: z.infer<typeof basicCourseInfoSchema>) {
     setCourseInfo(data);
     setCurrentStep((prev) => prev + 1);
     setPrevStep(currentStep);
   }
-  function handleCourseChapters(data: z.infer<typeof courseChaptersSchema>) {
-    console.log(data, courseInfo);
-    onCloseModal?.();
+  async function handleCourseChapters(data: z.infer<typeof courseChaptersSchema>) {
+    const toastId = toast.loading("Creating New Course...");
+    const chapters = data.chapters.map((chapter) => {
+      return { chapterName: chapter.title, chapterLink: chapter.link };
+    });
+    const newCourse = {
+      ...courseInfo,
+      chapters,
+      track: courseInfo?.trackName,
+    };
+    console.log(newCourse);
+    delete newCourse.image;
+    delete newCourse.trackName;
+    const { status, data: res } = await createCourse.mutateAsync(newCourse);
+    console.log(res);
+    if (status === "success") {
+      toast.success("Course created successfully", { id: toastId });
+      onCloseModal?.();
+    } else {
+      toast.error("Failed to create course", { id: toastId });
+    }
   }
   function handlePrev() {
     setCurrentStep((prev) => prev - 1);
@@ -26,9 +48,6 @@ export function AddCourseForm({ onCloseModal }: { onCloseModal?: () => void }) {
   }
   return (
     <main>
-      <header className="mb-5">
-        <h1 className="text-2xl font-semibold">Add a New Course</h1>
-      </header>
       <main>
         <AnimatePresence mode="wait">
           {currentStep === 0 && (
@@ -52,7 +71,11 @@ export function AddCourseForm({ onCloseModal }: { onCloseModal?: () => void }) {
               exit={{ x: `${currentStep < prevStep ? "" : "-"}100vw` }}
               transition={{ duration: 0.3 }}
             >
-              <CourseChaptersForm noChapters={8} handleCourseChapters={handleCourseChapters} onPrev={handlePrev} />
+              <CourseChaptersForm
+                noChapters={courseInfo?.noChapters || 1}
+                handleCourseChapters={handleCourseChapters}
+                onPrev={handlePrev}
+              />
             </motion.div>
           )}
         </AnimatePresence>
