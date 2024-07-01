4460c1fb039fc4c317a008e262f613fcf31a1729
kareem172 <kareemkhalaf1722@gmail.com>
2024-07-01 18:10:02 +0300

    feat: Add track name field to EducationForm

--- src/pages/mentor/beMentor/components/EducationForm.tsx
+++ src/pages/mentor/beMentor/components/EducationForm.tsx
@@ -3,10 +3,12 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import { PdfUploader } from "@/components/ui/PdfUploader";
+import Selector from "@/components/ui/Selector";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useTracks } from "@/hooks/useTracks";
 import { ProSectionSchema } from "@/schemas/mentorSchema";
 
 export function EducationForm({
@@ -27,6 +29,7 @@ export function EducationForm({
   function handleSubmit(formData: z.infer<typeof ProSectionSchema>) {
     onNext(formData);
   }
+  const tracks = useTracks();
   return (
     <Form {...form}>
       <form className={` min-w-full flex flex-col justify-between h-full`} onSubmit={form.handleSubmit(handleSubmit)}>
@@ -128,6 +131,25 @@ export function EducationForm({
           />
           <FormField
             control={form.control}
+            name="trackName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Track</FormLabel>
+                <FormControl>
+                  <Selector
+                    options={tracks.map((track) => track.name)}
+                    onChange={field.onChange}
+                    defaultValue={field.value}
+                    placeholder="Select a track"
+                    className="rounded-lg border-[0.1rem] border-zinc-400 bg-transparent py-2.5 pe-2.5 ps-4 outline-none placeholder:text-zinc-400 invalid:border-red-500 focus:border-white"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
             name="pricePerHour"
             render={({ field }) => (
               <FormItem>
@@ -159,7 +181,6 @@ export function EducationForm({
               </FormItem>
             )}
           />
-        </div>
           <div className="flex justify-end items-center gap-4 mt-8">
             <Button
               variant={"ghost"}
@@ -173,6 +194,7 @@ export function EducationForm({
               Next
             </Button>
           </div>
+        </div>
       </form>
     </Form>
   );
