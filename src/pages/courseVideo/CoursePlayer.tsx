63e92a6673e69e6625a590f0f501ba60ce20ea1d
kareem172 <kareemkhalaf1722@gmail.com>
2024-07-02 19:53:29 +0300

    feat: Update CoursePlayer component to use lazy loading for videos

--- src/pages/courseVideo/CoursePlayer.tsx
+++ src/pages/courseVideo/CoursePlayer.tsx
@@ -2,23 +2,28 @@ import { useSearchParams } from "react-router-dom";
 
 import videoPoster from "@/assets/videoPoster.jpg";
 import { Player } from "@/components/ui/player/player";
+import { getYoutubeId } from "@/utils/helpers";
 
 type CoursePlayerProps = {
   courseChapters: {
-    id: string;
-    name: string;
-    duration: string;
-    cLink: string;
+    id?: string;
+    chapterName: string;
+    chapterLink: string;
   }[];
 };
 export function CoursePlayer({ courseChapters }: CoursePlayerProps) {
   const [searchParams] = useSearchParams();
-  const videoId = searchParams.get("chapter") || "6410";
-  const currentVideo = courseChapters.find((chapter) => chapter.id === videoId);
+  const videoId = searchParams.get("chapter") || courseChapters[0].id;
+  const currentVideo = courseChapters.find((chapter) => chapter.id?.toString() === videoId);
+  const res = getYoutubeId(currentVideo?.chapterLink || "");
+  let videoLink = currentVideo?.chapterLink;
+  if (res) {
+    videoLink = `https://www.youtube.com/${res}?autoplay=0&showinfo=0&controls=0`;
+  }
   return (
     <Player
-      src={currentVideo?.cLink || ""}
-      title={currentVideo?.name}
+      src={videoLink || currentVideo?.chapterLink || ""}
+      title={currentVideo?.chapterName}
       poster={{ src: videoPoster, alt: "poster" }}
       className="sm:rounded-xl"
     />
