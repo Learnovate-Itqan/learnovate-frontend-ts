1577130bd279b24d6a9db191890b7d3f1738bb4d
kareem172 <kareemkhalaf1722@gmail.com>
2024-07-04 05:02:50 +0300

    refactor: Remove unused code and update StudentProfilePage layout

--- src/pages/profile/StudentProfilePage.tsx
+++ src/pages/profile/StudentProfilePage.tsx
@@ -1,12 +1,8 @@
 import { z } from "zod";
 
-import CourseCard from "@/components/ui/CourseCard";
 import { SocialCard } from "@/components/ui/SocialCard";
 import { SomethingWentWrong } from "@/components/ui/SomethingWentWrong";
-import { Spinner } from "@/components/ui/Spinner";
-import { Button } from "@/components/ui/button";
 import { useGetData } from "@/hooks/useApi";
-import { useWishlist } from "@/hooks/useWishlist";
 import { LoadingPage } from "@/layouts/LoadingPage";
 import { studentSchema } from "@/schemas/studentSchema";
 
@@ -14,10 +10,11 @@ import { BookedSessions } from "../mentor/me/components/BookedSessions";
 import { StudentHeader } from "./components/StudentHeader";
 import { StudentInfo } from "./components/StudentInfo";
 import { TracksProgress } from "./components/TracksProgress";
+import WishlistSection from "./components/WishlistSection";
 
 export function StudentProfilePage() {
   const { data: response, isLoading } = useGetData("students/profile");
-  const { wishlist, isLoading: isLoadingWishlist } = useWishlist();
+
   const { status, data } = response?.data || {};
   const { student }: { student: z.infer<typeof studentSchema> } = data || {};
 
@@ -56,74 +53,7 @@ export function StudentProfilePage() {
         <section className="py-10">
           <BookedSessions sessions={student.sessions || []} userRole="student" className="h-48" />
         </section>
-        <section className="py-10">
-          <div className="flex justify-between items-baseline">
-            <h1 className="text-3xl 3xl:text-4xl font-semibold">My Wishlist:</h1>
-            {wishlist.length > 3 && (
-              <Button className="text-lg md:text-xl 3xl:text-2xl" variant="link">
-                see more
-              </Button>
-            )}
-          </div>
-          <main className="grid grid-cols-auto-fit-19 xl:grid-cols-3 gap-5 mt-10 md:max-lg:last:*:col-span-2">
-            {isLoadingWishlist ? (
-              <Spinner />
-            ) : (
-              wishlist
-                ?.slice(0, 3)
-                .map((course) => (
-                  <CourseCard
-                    key={course.course.id}
-                    className=" min-w-72"
-                    name={course.course.title}
-                    rate={course.course.rating}
-                    description={course.course.description}
-                    level={course.course.cLevel}
-                    id={course.course.id}
-                    track={course.course.trackName}
-                    duration={course.course.estimatedTime || 0}
-                    image={course.course.image}
-                    price={course.course.price}
-                    trackId={course.course.trackID}
-                  />
-                ))
-            )}
-          </main>
-        </section>
-        <section className="py-10">
-          <div className="flex justify-between items-baseline">
-            <h1 className="text-3xl 3xl:text-4xl font-semibold">My Courses:</h1>
-            {wishlist.length > 3 && (
-              <Button className="text-lg md:text-xl 3xl:text-2xl" variant="link">
-                see more
-              </Button>
-            )}
-          </div>
-          <main className="grid grid-cols-auto-fit-19 xl:grid-cols-3 gap-5 mt-10 md:max-lg:last:*:col-span-2">
-            {isLoadingWishlist ? (
-              <Spinner />
-            ) : (
-              wishlist
-                ?.slice(0, 3)
-                .map((course) => (
-                  <CourseCard
-                    key={course.course.id}
-                    className=" min-w-72"
-                    name={course.course.title}
-                    rate={course.course.rating}
-                    description={course.course.description}
-                    level={course.course.cLevel}
-                    id={course.course.id}
-                    track={course.course.trackName}
-                    duration={course.course.estimatedTime || 0}
-                    image={course.course.image}
-                    price={course.course.price}
-                    trackId={course.course.trackID}
-                  />
-                ))
-            )}
-          </main>
-        </section>
+        <WishlistSection />
       </main>
     </main>
   );
